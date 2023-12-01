e6a58eb789b6af6c5076098c3fcc15e2c3e4e409
Vincent Oberle <vincent@oberle.org>
2023-12-01 14:47:25 +0200

    Small optimization

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ fn main() {
     let stdin = std::io::stdin();
     let mut total: u32 = 0;
     for line in stdin.lines() {
-        let s = line.unwrap();
-        let d: Vec<Option<u32>> = s.chars()
-            .filter(|c| c.is_digit(10))
-            .map(|c| c.to_digit(10))
+        let d: Vec<u32> = line
+            .unwrap()
+            .chars()
+            .filter_map(|c| c.to_digit(10))
             .collect();
         if !d.is_empty() {
-            let d1 = d.first().unwrap().unwrap();
-            let d2 = d.last().unwrap().unwrap();
+            let d1 = d.first().unwrap();
+            let d2 = d.last().unwrap();
             let line_total = d1 * 10 + d2;
-            dbg!(line_total);
+            //dbg!(line_total);
             total += line_total;
         }
     }
