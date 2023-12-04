9b63fb6a47fe00a482724f6a66d784061c90ab51
Vincent Oberle <vincent@oberle.org>
2023-12-04 16:52:30 +0200

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 // https://adventofcode.com/2023/day/4
 // Part 1 test: 13
 // Part 1: 21158
+// Part 2 test: 30
+// Part 2: 6050769
 
-use std::{collections::HashSet, io};
+use std::{collections::HashSet, io, usize};
+
+fn set_or_inc(v: &mut Vec<u32>, i: usize, val: u32) {
+    if v.get(i).is_some() {
+        v[i] += val;
+    } else {
+        v.insert(i, val);
+    }
+}
 
 fn main() {
     let mut total = 0;
+    let mut copies_count: Vec<u32> = Vec::new();
+    let mut i = 0;
     // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     for l in io::stdin().lines() {
         let line = l.unwrap();
@@ -18,9 +30,17 @@ fn main() {
             })
             .collect();
         let winning_number_count = sets[0].intersection(&sets[1]).count();
+
+        set_or_inc(&mut copies_count, i, 1);
+        for k in 0..winning_number_count {
+            let val = copies_count[i];
+            set_or_inc(&mut copies_count, i + k + 1, val);
+        }
         if winning_number_count > 0 {
             total += 2_u32.pow(winning_number_count as u32 - 1);
         }
+        i += 1;
     }
-    println!("{}", total);
+    println!("Part 1: {}", total);
+    println!("Part 2: {}", copies_count.iter().sum::<u32>());
 }
