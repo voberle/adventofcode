a35e5b0077ba1b35e53f58e0b26c280a8b60f2d6
Vincent Oberle <vincent@oberle.org>
2023-12-08 21:38:17 +0200

    Day 8 Part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -2,6 +2,7 @@
 // Part 1 test 1: 2
 // Part 1 test 2: 6
 // Part 1: 12643
+// Part 2: 13133452426987
 
 use regex::Regex;
 use std::{collections::HashMap, io};
@@ -28,23 +29,53 @@ fn main() {
         );
     }
 
-    let mut total_steps = 0;
-    let mut key = "AAA";
+    // Part 1
+    {
+        let mut total_steps = 0;
+        let mut key = "AAA";
+        for i in instructions.chars().cycle() {
+            //println!("'{i}' {key}");
+            let current_node = network.get(key).unwrap();
+            total_steps += 1;
 
-    for i in instructions.chars().cycle() {
-        //println!("'{i}' {key}");
-        let current_node = network.get(key).unwrap();
-        total_steps += 1;
-
-        key = if i == 'L' {
-            &current_node.0
-        } else {
-            &current_node.1
-        };
-        if key == "ZZZ" {
-            break;
+            key = if i == 'L' {
+                &current_node.0
+            } else {
+                &current_node.1
+            };
+            if key == "ZZZ" {
+                break;
+            }
         }
+        println!("Part 1: {}", total_steps);
     }
 
-    println!("Part 1: {}", total_steps);
+    // Part 2
+    {
+        let keys: Vec<String> = network.keys().filter(|k| k.ends_with('A')).cloned().collect();
+        let steps: Vec<u64> = keys.iter().map(|k| {
+            let mut steps_count = 0;
+            let mut key = k;
+            for i in instructions.chars().cycle() {
+                //println!("'{i}' {key}");
+                let current_node = network.get(key).unwrap();
+                steps_count += 1;
+
+                key = if i == 'L' {
+                    &current_node.0
+                } else {
+                    &current_node.1
+                };
+                if key.ends_with("Z") {
+                    break;
+                }
+            }
+            steps_count
+        })
+        .collect();
+
+        // I don't know why it's so..
+        let total_steps: u64 = steps.iter().fold(1, |n, i| num_integer::lcm(n, *i));
+        println!("Part 2: {}", total_steps);
+    }
 }
