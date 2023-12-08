e223895ce568e0470543b1751a6377e5b8008800
Vincent Oberle <vincent@oberle.org>
2023-12-08 21:09:32 +0200

    Day 8: Use cycle for the iterator

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -2,8 +2,6 @@
 // Part 1 test 1: 2
 // Part 1 test 2: 6
 // Part 1: 12643
-// Part 2 test 3: 6
-// Part 2: 
 
 use regex::Regex;
 use std::{collections::HashMap, io};
@@ -32,20 +30,19 @@ fn main() {
 
     let mut total_steps = 0;
     let mut key = "AAA";
-    'outer: loop {
-        for i in instructions.chars() {
-            //println!("'{i}' {key}");
-            let current_node = network.get(key).unwrap();
-            total_steps += 1;
 
-            key = if i == 'L' {
-                &current_node.0
-            } else {
-                &current_node.1
-            };
-            if key == "ZZZ" {
-                break 'outer;
-            }
+    for i in instructions.chars().cycle() {
+        //println!("'{i}' {key}");
+        let current_node = network.get(key).unwrap();
+        total_steps += 1;
+
+        key = if i == 'L' {
+            &current_node.0
+        } else {
+            &current_node.1
+        };
+        if key == "ZZZ" {
+            break;
         }
     }
 
