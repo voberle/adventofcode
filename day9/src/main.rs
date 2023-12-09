fe658a694cb1f3ebf700d8fd909bcaf85d822202
Vincent Oberle <vincent@oberle.org>
2023-12-09 23:03:03 +0200

    Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,8 @@
 // https://adventofcode.com/2023/day/9
 // Part 1 test: 114
 // Part 1: 1974232246
+// Part 2 test: 2
+// Part 2: 928
 
 use std::io;
 
@@ -16,21 +18,28 @@ fn main() {
         })
         .collect();
 
-    let mut sum_extrapolated_values = 0;
+    let mut sum_extrapolated_values_begin = 0;
+    let mut sum_extrapolated_values_end = 0;
     for history in report {
+        let mut first_item: Vec<i32> = Vec::new();
         let mut last_item: Vec<i32> = Vec::new();
         let mut next_line: Vec<i32> = history.clone();
-        println!("{:?}", next_line);
+        // println!("{:?}", next_line);
         while next_line.iter().any(|i| *i != 0) {
+            first_item.push(*next_line.first().unwrap());
             last_item.push(*next_line.last().unwrap());
             next_line = next_line.windows(2).map(|w| w[1] - w[0]).collect();
-            println!("{:?}", next_line);
+            // println!("{:?}", next_line);
         }
-        println!("last_item {:?}", last_item);
 
-        let next_val: i32 = last_item.iter().sum();
-        println!("Next val {}", next_val);
-        sum_extrapolated_values += next_val;
+        sum_extrapolated_values_end += last_item.iter().sum::<i32>();
+
+        first_item.reverse();
+        // println!("first_item {:?}", first_item);
+        let first_val = first_item.iter().fold(0, |n, i| i - n);
+        // println!("first_val {}", first_val);
+        sum_extrapolated_values_begin += first_val;
     }
-    println!("Part 1: {}", sum_extrapolated_values);
+    println!("Part 1: {}", sum_extrapolated_values_end);
+    println!("Part 2: {}", sum_extrapolated_values_begin);
 }
