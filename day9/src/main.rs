c3dd52b7c22c352c2f0c6a50d9bb925236f69680
Vincent Oberle <vincent@oberle.org>
2023-12-09 22:48:07 +0200

    Day 2 part 1: Fix bug

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2023/day/9
 // Part 1 test: 114
-// Part 1: 1974232257
+// Part 1: 1974232246
 
 use std::io;
 
@@ -15,22 +15,19 @@ fn main() {
                 .collect()
         })
         .collect();
-    // println!("{:#?}", report);
 
     let mut sum_extrapolated_values = 0;
     for history in report {
         let mut last_item: Vec<i32> = Vec::new();
         let mut next_line: Vec<i32> = history.clone();
         println!("{:?}", next_line);
-        while next_line.iter().sum::<i32>() != 0 {
+        while next_line.iter().any(|i| *i != 0) {
             last_item.push(*next_line.last().unwrap());
             next_line = next_line.windows(2).map(|w| w[1] - w[0]).collect();
             println!("{:?}", next_line);
         }
-        last_item.reverse();
         println!("last_item {:?}", last_item);
 
-        // let next_val = last_item.iter().fold(0, |n, i| n + i);
         let next_val: i32 = last_item.iter().sum();
         println!("Next val {}", next_val);
         sum_extrapolated_values += next_val;
