12648573df5a0ba6613f57e5c6f37b7172991720
Vincent Oberle <vincent@oberle.org>
2023-12-14 11:19:43 +0200

    Day 14 Part 1: Works

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,4 +1,5 @@
 // https://adventofcode.com/2023/day/14
+// Part 1: 113486
 
 use std::io::{self, BufRead};
 
@@ -59,27 +60,75 @@ impl Platform {
         p
     }
 
-    fn total_load_north(&self) -> u32 {
-        0
+    fn collapse_north(&self) -> Self {
+        let mut collapsed_columns: Vec<Vec<char>> = (0..self.height)
+            .map(|c| collapse_down(&self.col(c)))
+            .collect();
+        // println!("collapsed_columns {:?}", collapsed_columns);
+
+        let mut result = Platform::empty();
+        result.width = self.width;
+        result.height = self.height;
+        for i in 0..self.height {
+            let line = collapsed_columns.iter().map(|l| l[i]).collect::<Vec<_>>();
+            // println!("LINE {:?}", line);
+            result.arr.extend(line.iter());
+        }
+        result
+    }
+
+    fn total_load_north(&self) -> usize {
+        self.collapse_north()
+            .arr
+            .iter()
+            .enumerate()
+            .filter(|(i, c)| **c == 'O')
+            .map(|(i, _)| self.height - i / self.width)
+            .sum()
     }
 }
 
-#[test]
-fn test_line_row() {
-    let p = Platform::new("1234567890ab".chars().collect(), 4, 3);
-    assert_eq!(p.line(0), "1234".chars().collect::<Vec<_>>());
-    assert_eq!(p.line(1), "5678".chars().collect::<Vec<_>>());
-    assert_eq!(p.line(2), "90ab".chars().collect::<Vec<_>>());
-    assert_eq!(p.col(0), "159".chars().collect::<Vec<_>>());
-    assert_eq!(p.col(1), "260".chars().collect::<Vec<_>>());
-    assert_eq!(p.col(2), "37a".chars().collect::<Vec<_>>());
-    assert_eq!(p.col(3), "48b".chars().collect::<Vec<_>>());
+// Collapsing one line of rocks
+fn collapse_down(s: &[char]) -> Vec<char> {
+    let mut res: Vec<char> = Vec::with_capacity(s.len());
+
+    let mut dot_cnt = 0;
+    let mut O_cnt = 0;
+    for c in s {
+        match c {
+            '#' => {
+                for _ in 0..O_cnt {
+                    res.push('O')
+                }
+                O_cnt = 0;
+                for _ in 0..dot_cnt {
+                    res.push('.')
+                }
+                dot_cnt = 0;
+                res.push(*c);
+            }
+            '.' => {
+                dot_cnt += 1;
+            }
+            'O' => {
+                O_cnt += 1;
+            }
+            _ => {}
+        }
+    }
+    for _ in 0..O_cnt {
+        res.push('O')
+    }
+    for _ in 0..dot_cnt {
+        res.push('.')
+    }
+    res
 }
 
 fn main() {
     let stdin = io::stdin();
     let platform = Platform::build(&mut stdin.lock());
-    platform.println();
+    // platform.println();
 
     println!("Part 1: {}", platform.total_load_north());
 }
@@ -89,15 +138,52 @@ pub mod tests {
     use super::*;
     use std::{fs::File, io::BufReader};
 
-    fn part1(filename: &str) -> u32 {
-        let file = File::open(filename).unwrap();
-        let mut reader = BufReader::new(file);
-        let platform = Platform::build(&mut reader);
-        platform.total_load_north()
+    #[test]
+    fn test_line_row() {
+        let p = Platform::new("1234567890ab".chars().collect(), 4, 3);
+        assert_eq!(p.line(0), "1234".chars().collect::<Vec<_>>());
+        assert_eq!(p.line(1), "5678".chars().collect::<Vec<_>>());
+        assert_eq!(p.line(2), "90ab".chars().collect::<Vec<_>>());
+        assert_eq!(p.col(0), "159".chars().collect::<Vec<_>>());
+        assert_eq!(p.col(1), "260".chars().collect::<Vec<_>>());
+        assert_eq!(p.col(2), "37a".chars().collect::<Vec<_>>());
+        assert_eq!(p.col(3), "48b".chars().collect::<Vec<_>>());
     }
 
     #[test]
+    fn test_collapse_down() {
+        assert_eq!(
+            collapse_down(&"..#...O.#.".chars().collect::<Vec<_>>()),
+            "..#O....#.".chars().collect::<Vec<_>>()
+        );
+        assert_eq!(
+            collapse_down(&"..O...O.".chars().collect::<Vec<_>>()),
+            "OO......".chars().collect::<Vec<_>>()
+        );
+        assert_eq!(
+            collapse_down(&"OO.O.O..##".chars().collect::<Vec<_>>()),
+            "OOOO....##".chars().collect::<Vec<_>>()
+        );
+        assert_eq!(
+            collapse_down(&".#.O.#O...".chars().collect::<Vec<_>>()),
+            ".#O..#O...".chars().collect::<Vec<_>>()
+        );
+    }
+    #[test]
     fn test_part1() {
-        assert_eq!(part1("resources/input_test"), 136);
+        let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
+        let platform = Platform::build(&mut reader);
+        platform.println();
+
+        let platform_collapsed = platform.collapse_north();
+        platform_collapsed.println();
+
+        let mut reader_res = BufReader::new(File::open("resources/result_test").unwrap());
+        let platform_res = Platform::build(&mut reader_res);
+        platform_res.println();
+
+        assert_eq!(platform_collapsed, platform_res);
+
+        assert_eq!(platform.total_load_north(), 136);
     }
 }
