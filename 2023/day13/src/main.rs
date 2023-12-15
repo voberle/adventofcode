780a2f3c93f7411bc55baa3ddccb85810d067f2a
Vincent Oberle <vincent@oberle.org>
2023-12-15 19:28:12 +0200

    Libs: Convert day 13 and 14 to table library

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -1,6 +1,7 @@
 // https://adventofcode.com/2023/day/13
 
-use std::io::{self, BufRead};
+use std::io;
+use table::{build_tables, Table};
 
 #[derive(Debug)]
 enum Reflection {
@@ -17,99 +18,47 @@ impl Reflection {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Pattern {
-    // String would be better for printing, but harder for index accessing
-    arr: Vec<char>,
-    width: usize,
-    height: usize,
+fn find_vertical_reflexion(table: &Table<char>, refl_to_ignore: &Option<Reflection>) -> Option<usize> {
+    let mut to_check: Vec<usize> = (0..table.width - 1).collect::<Vec<_>>();
+    for row in 0..table.height {
+        to_check = find_reflexions_for_line(table.row(row), &to_check);
+    }
+    if let Some(Reflection::Vertical(refl)) = refl_to_ignore {
+        to_check.iter().filter(|val| *val != refl).next().copied()
+    } else {
+        to_check.first().copied()
+    }
 }
 
-impl Pattern {
-    fn new(arr: Vec<char>, width: usize, height: usize) -> Self {
-        Self { arr, width, height }
+fn find_horizontal_reflexion(table: &Table<char>, refl_to_ignore: &Option<Reflection>) -> Option<usize> {
+    let mut to_check: Vec<usize> = (0..table.height - 1).collect::<Vec<_>>();
+    for col in 0..table.width {
+        to_check = find_reflexions_for_line(&table.col(col), &to_check);
     }
+    if let Some(Reflection::Horizontal(refl)) = refl_to_ignore {
+        to_check.iter().filter(|val| *val != refl).next().copied()
+    } else {
+        to_check.first().copied()
+    }
+}
 
-    fn empty() -> Self {
-        Self {
-            arr: Vec::new(),
-            width: 0,
-            height: 0,
-        }
-    }
+fn find_reflection(table: &Table<char>) -> Option<Reflection> {
+    find_reflection_with_ignore(table, &None)
+}
 
-    fn line(&self, row: usize) -> &[char] {
-        let idx = row * self.width;
-        &self.arr[idx..idx + self.width]
+// In part 2, the original reflection may still be valid, so we need to ignore it
+// in order to find the other one always.
+fn find_reflection_with_ignore(
+    table: &Table<char>,
+    refl_to_ignore: &Option<Reflection>,
+) -> Option<Reflection> {
+    if let Some(c) = find_vertical_reflexion(table, refl_to_ignore) {
+        return Some(Reflection::Vertical(c));
     }
-
-    fn row(&self, col: usize) -> Vec<char> {
-        // Much less efficient than line unfortunately
-        self.arr
-            .iter()
-            .skip(col)
-            .step_by(self.width)
-            .cloned()
-            .collect::<Vec<_>>()
+    if let Some(r) = find_horizontal_reflexion(table, refl_to_ignore) {
+        return Some(Reflection::Horizontal(r));
     }
-
-    fn println(&self) {
-        println!("Cols={}; Rows={}", self.height, self.width);
-        for row in 0..self.height {
-            // very ineficient :-(
-            println!(
-                "{}",
-                self.line(row)
-                    .iter()
-                    .map(char::to_string)
-                    .collect::<Vec<String>>()
-                    .join("")
-            );
-        }
-    }
-
-    fn find_vertical_reflexion(&self, refl_to_ignore: &Option<Reflection>) -> Option<usize> {
-        let mut to_check: Vec<usize> = (0..self.width - 1).collect::<Vec<_>>();
-        for row in 0..self.height {
-            to_check = find_reflexions_for_line(self.line(row), &to_check);
-        }
-        if let Some(Reflection::Vertical(refl)) = refl_to_ignore {
-            to_check.iter().filter(|val| *val != refl).next().copied()
-        } else {
-            to_check.first().copied()
-        }
-    }
-
-    fn find_horizontal_reflexion(&self, refl_to_ignore: &Option<Reflection>) -> Option<usize> {
-        let mut to_check: Vec<usize> = (0..self.height - 1).collect::<Vec<_>>();
-        for col in 0..self.width {
-            to_check = find_reflexions_for_line(&self.row(col), &to_check);
-        }
-        if let Some(Reflection::Horizontal(refl)) = refl_to_ignore {
-            to_check.iter().filter(|val| *val != refl).next().copied()
-        } else {
-            to_check.first().copied()
-        }
-    }
-
-    fn find_reflection(&self) -> Option<Reflection> {
-        self.find_reflection_with_ignore(&None)
-    }
-
-    // In part 2, the original reflection may still be valid, so we need to ignore it
-    // in order to find the other one always.
-    fn find_reflection_with_ignore(
-        &self,
-        refl_to_ignore: &Option<Reflection>,
-    ) -> Option<Reflection> {
-        if let Some(c) = self.find_vertical_reflexion(refl_to_ignore) {
-            return Some(Reflection::Vertical(c));
-        }
-        if let Some(r) = self.find_horizontal_reflexion(refl_to_ignore) {
-            return Some(Reflection::Horizontal(r));
-        }
-        None
-    }
+    None
 }
 
 // Finds a reflexion point for a line.
@@ -152,28 +101,6 @@ fn find_reflexions_for_line(line: &[char], to_check: &[usize]) -> Vec<usize> {
 }
 
 #[test]
-fn test_line() {
-    let p = Pattern::new("#.##..##...#.##.#.##......#".chars().collect(), 9, 3);
-    assert_eq!(p.line(0), "#.##..##.".chars().collect::<Vec<_>>());
-    assert_eq!(p.line(1), "..#.##.#.".chars().collect::<Vec<_>>());
-    assert_eq!(p.line(2), "##......#".chars().collect::<Vec<_>>());
-}
-
-#[test]
-fn test_row() {
-    let p = Pattern::new("#.##..##...#.##.#.##......#".chars().collect(), 9, 3);
-    assert_eq!(p.row(0), "#.#".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(1), "..#".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(2), "##.".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(3), "#..".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(4), ".#.".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(5), ".#.".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(6), "#..".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(7), "##.".chars().collect::<Vec<_>>());
-    assert_eq!(p.row(8), "..#".chars().collect::<Vec<_>>());
-}
-
-#[test]
 fn test_find_reflexions_for_line() {
     let line: Vec<char> = "#.##..##.".chars().collect();
     assert_eq!(
@@ -188,44 +115,23 @@ fn test_find_reflexions_for_line() {
     );
 }
 
-fn build_patterns<R>(reader: &mut R) -> Vec<Pattern>
-where
-    R: BufRead,
-{
-    let mut patterns: Vec<Pattern> = Vec::new();
-    let mut p = Pattern::empty();
-    for l in reader.lines() {
-        let line = l.unwrap();
-        if line.is_empty() {
-            patterns.push(p);
-            p = Pattern::empty();
-        } else {
-            p.arr.extend(line.chars());
-            p.width = line.len();
-            p.height += 1;
-        }
-    }
-    patterns.push(p); // not forgetting last one
-    patterns
-}
-
-fn find_summary(patterns: &[Pattern]) -> usize {
+fn find_summary(patterns: &[Table<char>]) -> usize {
     patterns
         .iter()
-        .map(|p| p.find_reflection())
+        .map(|p| find_reflection(p))
         .map(|e| e.map_or(0, |r| r.summary()))
         .sum()
 }
 
-fn find_summary_with_smudges(patterns: &[Pattern]) -> usize {
+fn find_summary_with_smudges(patterns: &[Table<char>]) -> usize {
     patterns
         .iter()
         .map(|p| {
-            let original_reflection = p.find_reflection();
+            let original_reflection = find_reflection(p);
             p.arr.iter().enumerate().find_map(|(i, smudge)| {
-                let mut repaired: Pattern = p.clone();
+                let mut repaired: Table<char> = p.clone();
                 repaired.arr[i] = if *smudge == '.' { '#' } else { '.' };
-                repaired.find_reflection_with_ignore(&original_reflection)
+                find_reflection_with_ignore(&repaired, &original_reflection)
             })
         })
         .map(|o| o.unwrap().summary())
@@ -235,26 +141,26 @@ fn find_summary_with_smudges(patterns: &[Pattern]) -> usize {
 fn main() {
     let stdin = io::stdin();
 
-    let patterns: Vec<Pattern> = build_patterns(&mut stdin.lock());
-    // for p in &patterns {
-    //     p.println();
-    //     p.find_vertical_reflexion();
-    //     println!();
-    // }
-    // println!("{:?}", patterns);
+    let patterns: Vec<Table<char>> = build_tables(&mut stdin.lock());
+    for p in &patterns {
+        println!("{}", p);
+    }
 
     println!("Part 1: {}", find_summary(&patterns));
     println!("Part 2: {}", find_summary_with_smudges(&patterns));
 }
 
 #[cfg(test)]
-use std::{fs::File, io::BufReader};
+pub mod tests {
+    use super::*;
+    use std::{fs::File, io::BufReader};
 
-#[test]
-fn test_data() {
-    let file = File::open("resources/input_test").unwrap();
-    let mut reader = BufReader::new(file);
-    let records: Vec<Pattern> = build_patterns(&mut reader);
-    assert_eq!(find_summary(&records), 405);
-    assert_eq!(find_summary_with_smudges(&records), 400);
+    #[test]
+    fn test_data() {
+        let file = File::open("resources/input_test").unwrap();
+        let mut reader = BufReader::new(file);
+        let records: Vec<Table<char>> = build_tables(&mut reader);
+        assert_eq!(find_summary(&records), 405);
+        assert_eq!(find_summary_with_smudges(&records), 400);
+    }
 }
--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -1,154 +1,98 @@
 // https://adventofcode.com/2023/day/14
 
-use std::io::{self, BufRead};
+use std::io;
+use table::Table;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Platform {
-    arr: Vec<char>,
-    width: usize,
-    height: usize,
+fn collapse_north(table: &Table<char>) -> Table<char> {
+    let collapsed_columns: Vec<Vec<char>> = (0..table.height)
+        .map(|c| collapse_down(&table.col(c)))
+        .collect();
+
+    let mut result = Table::empty();
+    result.width = table.width;
+    result.height = table.height;
+    for i in 0..table.height {
+        let line = collapsed_columns.iter().map(|l| l[i]).collect::<Vec<_>>();
+        result.arr.extend(line.iter());
+    }
+    result
 }
 
-// TODO all the initial code below is generic, make it reusable
-impl Platform {
-    fn new(arr: Vec<char>, width: usize, height: usize) -> Self {
-        Self { arr, width, height }
-    }
+fn collapse_south(table: &Table<char>) -> Table<char> {
+    let collapsed_columns: Vec<Vec<char>> = (0..table.height)
+        .map(|c| {
+            let mut col = table.col(c);
+            col.reverse();
+            let mut collapsed = collapse_down(&col);
+            collapsed.reverse();
+            collapsed
+        })
+        .collect();
 
-    fn empty() -> Self {
-        Self {
-            arr: Vec::new(),
-            width: 0,
-            height: 0,
-        }
-    }
-
-    fn row(&self, row: usize) -> &[char] {
-        let idx = row * self.width;
-        &self.arr[idx..idx + self.width]
-    }
-
-    fn col(&self, col: usize) -> Vec<char> {
-        // Much less efficient than line unfortunately
-        self.arr
+    let mut result = Table::empty();
+    result.width = table.width;
+    result.height = table.height;
+    for i in 0..table.height {
+        let line = collapsed_columns
             .iter()
-            .skip(col)
-            .step_by(self.width)
-            .cloned()
-            .collect::<Vec<_>>()
+            .map(|l| l[table.height - i - 1])
+            .collect::<Vec<_>>();
+        result.arr.extend(line.iter());
     }
+    result
+}
 
-    fn println(&self) {
-        println!("Cols={}; Rows={}", self.height, self.width);
-        for row in 0..self.height {
-            println!("{}", self.row(row).iter().collect::<String>());
-        }
+fn collapse_west(table: &Table<char>) -> Table<char> {
+    let collapsed_columns: Vec<Vec<char>> = (0..table.width)
+        .map(|r| collapse_down(&table.row(r)))
+        .collect();
+
+    let mut result = Table::empty();
+    result.width = table.width;
+    result.height = table.height;
+    for i in 0..table.width {
+        result.arr.extend(collapsed_columns[i].iter());
     }
+    result
+}
 
-    fn build<R>(reader: &mut R) -> Self
-    where
-        R: BufRead,
-    {
-        let mut p = Platform::empty();
-        for l in reader.lines() {
-            let line = l.unwrap();
-            p.arr.extend(line.chars());
-            p.width = line.len();
-            p.height += 1;
-        }
-        p
-    }
+fn collapse_east(table: &Table<char>) -> Table<char> {
+    let collapsed_columns: Vec<Vec<char>> = (0..table.width)
+        .map(|r| {
+            let row = &mut table.row(r).to_vec();
+            row.reverse();
+            let mut collapsed = collapse_down(&row);
+            collapsed.reverse();
+            collapsed
+        })
+        .collect();
 
-    fn collapse_north(&self) -> Self {
-        let collapsed_columns: Vec<Vec<char>> = (0..self.height)
-            .map(|c| collapse_down(&self.col(c)))
-            .collect();
-
-        let mut result = Platform::empty();
-        result.width = self.width;
-        result.height = self.height;
-        for i in 0..self.height {
-            let line = collapsed_columns.iter().map(|l| l[i]).collect::<Vec<_>>();
-            result.arr.extend(line.iter());
-        }
+    let mut result = Table::empty();
+    result.width = table.width;
+    result.height = table.height;
+    for i in 0..table.width {
         result
+            .arr
+            .extend(collapsed_columns[table.width - i - 1].iter());
     }
+    result
+}
 
-    fn collapse_south(&self) -> Self {
-        let collapsed_columns: Vec<Vec<char>> = (0..self.height)
-            .map(|c| {
-                let mut col = self.col(c);
-                col.reverse();
-                let mut collapsed = collapse_down(&col);
-                collapsed.reverse();
-                collapsed
-            })
-            .collect();
+fn cycle(table: &Table<char>) -> Table<char> {
+    let mut t = collapse_north(table);
+    t = collapse_west(&t);
+    t = collapse_south(&t);
+    collapse_east(&t)
+}
 
-        let mut result = Platform::empty();
-        result.width = self.width;
-        result.height = self.height;
-        for i in 0..self.height {
-            let line = collapsed_columns
-                .iter()
-                .map(|l| l[self.height - i - 1])
-                .collect::<Vec<_>>();
-            result.arr.extend(line.iter());
-        }
-        result
-    }
-
-    fn collapse_west(&self) -> Self {
-        let collapsed_columns: Vec<Vec<char>> = (0..self.width)
-            .map(|r| collapse_down(&self.row(r)))
-            .collect();
-
-        let mut result = Platform::empty();
-        result.width = self.width;
-        result.height = self.height;
-        for i in 0..self.width {
-            result.arr.extend(collapsed_columns[i].iter());
-        }
-        result
-    }
-
-    fn collapse_east(&self) -> Self {
-        let collapsed_columns: Vec<Vec<char>> = (0..self.width)
-            .map(|r| {
-                let row = &mut self.row(r).to_vec();
-                row.reverse();
-                let mut collapsed = collapse_down(&row);
-                collapsed.reverse();
-                collapsed
-            })
-            .collect();
-
-        let mut result = Platform::empty();
-        result.width = self.width;
-        result.height = self.height;
-        for i in 0..self.width {
-            result
-                .arr
-                .extend(collapsed_columns[self.width - i - 1].iter());
-        }
-        result
-    }
-
-    fn cycle(&self) -> Self {
-        self.collapse_north()
-            .collapse_west()
-            .collapse_south()
-            .collapse_east()
-    }
-
-    fn total_load_north(&self) -> usize {
-        self.arr
-            .iter()
-            .enumerate()
-            .filter(|(_, c)| **c == 'O')
-            .map(|(i, _)| self.height - i / self.width)
-            .sum()
-    }
+fn total_load_north(table: &Table<char>) -> usize {
+    table
+        .arr
+        .iter()
+        .enumerate()
+        .filter(|(_, c)| **c == 'O')
+        .map(|(i, _)| table.height - i / table.width)
+        .sum()
 }
 
 // Collapsing one line of rocks
@@ -188,25 +132,25 @@ fn collapse_down(s: &[char]) -> Vec<char> {
     res
 }
 
-fn cycle_nth(platform: Platform, count: usize) -> Platform {
+fn cycle_nth(platform: Table<char>, count: usize) -> Table<char> {
     let mut p = platform;
     for _ in 0..count {
-        p = p.cycle();
+        p = cycle(&p);
     }
     p
 }
 
 // The cycle repeats at some point, finding when.
 // Returns the value and its period.
-fn find_period(platform: Platform, warmup: usize) -> (usize, Platform) {
+fn find_period(platform: Table<char>, warmup: usize) -> (usize, Table<char>) {
     let initial_pattern = cycle_nth(platform, warmup);
     println!("initial_pattern: {:?}", initial_pattern);
     let mut p = initial_pattern.clone();
     let mut i = 0;
     loop {
         i += 1;
-        p = p.cycle();
-        println!("{} cycles: {}", i, p.total_load_north());
+        p = cycle(&p);
+        println!("{} cycles: {}", i, total_load_north(&p));
         if p == initial_pattern {
             break;
         }
@@ -215,7 +159,7 @@ fn find_period(platform: Platform, warmup: usize) -> (usize, Platform) {
     (i, p)
 }
 
-fn total_load_north_after_n_cycles(platform: Platform, cycles: usize) -> usize {
+fn total_load_north_after_n_cycles(platform: Table<char>, cycles: usize) -> usize {
     const WARMUP: usize = 100;
 
     let (period, mut p) = find_period(platform, WARMUP);
@@ -224,16 +168,16 @@ fn total_load_north_after_n_cycles(platform: Platform, cycles: usize) -> usize {
     let c = WARMUP + ((cycles - WARMUP) / period) * period;
     // println!("period={}, c={}", period, c);
     for _ in c..cycles {
-        p = p.cycle();
+        p = cycle(&p);
     }
-    p.total_load_north()
+    total_load_north(&p)
 }
 
 fn main() {
     let stdin = io::stdin();
-    let platform = Platform::build(&mut stdin.lock());
+    let platform = Table::build(&mut stdin.lock());
 
-    println!("Part 1: {}", platform.collapse_north().total_load_north());
+    println!("Part 1: {}", total_load_north(&collapse_north(&platform)));
 
     println!(
         "Part 2: {}",
@@ -245,18 +189,6 @@ fn main() {
 pub mod tests {
     use super::*;
     use std::{fs::File, io::BufReader};
-
-    #[test]
-    fn test_line_row() {
-        let p = Platform::new("1234567890ab".chars().collect(), 4, 3);
-        assert_eq!(p.row(0), "1234".chars().collect::<Vec<_>>());
-        assert_eq!(p.row(1), "5678".chars().collect::<Vec<_>>());
-        assert_eq!(p.row(2), "90ab".chars().collect::<Vec<_>>());
-        assert_eq!(p.col(0), "159".chars().collect::<Vec<_>>());
-        assert_eq!(p.col(1), "260".chars().collect::<Vec<_>>());
-        assert_eq!(p.col(2), "37a".chars().collect::<Vec<_>>());
-        assert_eq!(p.col(3), "48b".chars().collect::<Vec<_>>());
-    }
 
     #[test]
     fn test_collapse_down() {
@@ -281,42 +213,44 @@ pub mod tests {
     #[test]
     fn test_part1() {
         let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
-        let platform = Platform::build(&mut reader);
-        platform.println();
+        let platform = Table::build(&mut reader);
+        println!("{}", platform);
 
-        let platform_collapsed = platform.collapse_north();
-        platform_collapsed.println();
+        let platform_collapsed = collapse_north(&platform);
+        println!("{}", platform_collapsed);
 
         let mut reader_res = BufReader::new(File::open("resources/result_test").unwrap());
-        let platform_res = Platform::build(&mut reader_res);
-        platform_res.println();
+        let platform_res = Table::build(&mut reader_res);
+        println!("{}", platform_res);
 
         assert_eq!(platform_collapsed, platform_res);
 
-        assert_eq!(platform_collapsed.total_load_north(), 136);
+        assert_eq!(total_load_north(&platform_collapsed), 136);
     }
 
     #[test]
     fn test_part2() {
         let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
-        let platform = Platform::build(&mut reader);
-        platform.println();
+        let platform = Table::build(&mut reader);
+        println!("{}", platform);
 
-        let platform_1cycle = platform.cycle();
-        platform_1cycle.println();
+        let platform_1cycle = cycle(&platform);
+        println!("{}", platform_1cycle);
 
         let mut reader_res = BufReader::new(File::open("resources/result_test_1cycle").unwrap());
-        let platform_res = Platform::build(&mut reader_res);
-        platform_res.println();
+        let platform_res = Table::build(&mut reader_res);
+        println!("{}", platform_res);
 
         assert_eq!(platform_1cycle, platform_res);
 
-        let platform_3cycle = platform.cycle().cycle().cycle();
-        platform_3cycle.println();
+        let mut platform_3cycle = cycle(&platform);
+        platform_3cycle = cycle(&platform_3cycle);
+        platform_3cycle = cycle(&platform_3cycle);
+        println!("{}", platform_3cycle);
 
         let mut reader_res3 = BufReader::new(File::open("resources/result_test_3cycle").unwrap());
-        let platform_res3 = Platform::build(&mut reader_res3);
-        platform_res.println();
+        let platform_res3 = Table::build(&mut reader_res3);
+        println!("{}", platform_res);
 
         assert_eq!(platform_3cycle, platform_res3);
 
