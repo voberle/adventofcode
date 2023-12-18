d0d67ed3ee4adc2a1f94b3f54c9c2adfbec31dc1
Vincent Oberle <vincent@oberle.org>
2023-12-18 17:50:24 +0200

    Day 18 Part 2: Working

--- 2023/day18/src/main.rs
+++ 2023/day18/src/main.rs
@@ -4,12 +4,12 @@ use std::{io::{self, BufRead}, collections::{VecDeque, HashSet}};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Pos {
-    row: i32,
-    col: i32,
+    row: i64,
+    col: i64,
 }
 
 impl Pos {
-    fn new(row: i32, col: i32) -> Self {
+    fn new(row: i64, col: i64) -> Self {
         Self { row, col }
     }
 
@@ -31,15 +31,15 @@ impl Pos {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Instruction {
     direction: char,
-    meters: i32,
+    meters: i64,
     color: String,
 }
 
 impl Instruction {
-    fn new(direction: char, meters: i32, color: String) -> Self {
+    fn new(direction: char, meters: i64, color: String) -> Self {
         Self {
             direction,
             meters,
@@ -52,12 +52,12 @@ impl Instruction {
         Self {
             direction: parts[0].chars().next().unwrap(),
             meters: parts[1].parse().unwrap(),
-            color: parts[2].to_string(),
+            color: parts[2][2..parts[2].len()-1].to_string(),
         }
     }
 
     // Digs these instructions. The new position is the last item of the result.
-    fn dig(&self, start: &Pos) -> Vec<Pos> {
+    fn _dig(&self, start: &Pos) -> Vec<Pos> {
         let range = 1..self.meters + 1;
         match self.direction {
             'U' => range.map(|i| Pos::new(start.row - i, start.col)).collect(),
@@ -67,99 +67,184 @@ impl Instruction {
             _ => panic!("Invalid direction char {}", self.direction),
         }
     }
+
+    // Digs these instructions and return the new position.
+    fn dig(&self, start: &Pos) -> Pos {
+        match self.direction {
+            'U' => Pos::new(start.row - self.meters, start.col),
+            'D' => Pos::new(start.row + self.meters, start.col),
+            'L' => Pos::new(start.row, start.col - self.meters),
+            'R' => Pos::new(start.row, start.col + self.meters),
+            _ => panic!("Invalid direction char {}", self.direction),
+        }
+    }
+
+    fn invert(&self) -> Self {
+        let meters = i64::from_str_radix(&self.color[0..self.color.len()-1], 16).unwrap();
+        let dir_char = &self.color.chars().last().unwrap(); //[self.color.len()-2..self.color.len()-2];
+        let direction = match dir_char {
+            '0' => 'R',
+            '1' => 'D',
+            '2' => 'L',
+            '3' => 'U',
+            _ =>  panic!("Invalid direction char in color {}: '{}'", self.color, dir_char),
+        };
+        Self::new(direction, meters, String::new())
+    }
 }
 
 #[test]
 fn test_instruction_dig() {
     let start = Pos::new(0, 0);
     let ins = Instruction::build("R 2 (#70c710)");
-    assert_eq!(ins.dig(&start), vec![Pos::new(0, 1), Pos::new(0, 2)])
+    assert_eq!(ins.dig(&start), Pos::new(0, 2))
 }
 
-fn dig(dig_plan: &Vec<Instruction>) -> Vec<Pos> {
+#[test]
+fn test_instruction_invert() {
+    let ins = Instruction::build("R 2 (#70c710)");
+    assert_eq!(ins.invert(), Instruction::new('R', 461937, "".to_string()));
+}
+
+fn _dig(dig_plan: &Vec<Instruction>) -> Vec<Pos> {
     let mut trench: Vec<Pos> = Vec::new();
     let mut current = &Pos::new(0, 0);
     for ins in dig_plan {
-        trench.extend(ins.dig(current));
+        trench.extend(ins._dig(current));
         current = trench.last().unwrap();
     }
     trench
 }
 
-fn min_max_of_trench(trench: &[Pos]) -> (Pos, Pos) {
+// Digs the trench, and return a list of vertices.
+// A vertice is a pair of coordinates representing a side of the polygon.
+fn dig(dig_plan: &Vec<Instruction>) -> Vec<(Pos, Pos)> {
+    let mut trench: Vec<(Pos, Pos)> = Vec::new();
+    let mut current = Pos::new(0, 0);
+    for ins in dig_plan {
+        let next_corner = ins.dig(&current);
+        trench.push((current, next_corner.clone()));
+        current = next_corner;
+    }
+    trench
+}
+
+fn min_max_of_trench(trench: &[(Pos, Pos)]) -> (Pos, Pos) {
     assert!(!trench.is_empty());
     (
         Pos::new(
-            trench.iter().map(|p| p.row).min().unwrap(),
-            trench.iter().map(|p| p.col).min().unwrap()
+            trench.iter().map(|(p1, p2)| i64::min(p1.row, p2.row)).min().unwrap(),
+            trench.iter().map(|(p1, p2)| i64::min(p1.col, p2.col)).min().unwrap()
         ),
         Pos::new(
-            trench.iter().map(|p| p.row).max().unwrap(),
-            trench.iter().map(|p| p.col).max().unwrap()
+            trench.iter().map(|(p1, p2)| i64::min(p1.row, p2.row)).max().unwrap(),
+            trench.iter().map(|(p1, p2)| i64::min(p1.col, p2.col)).max().unwrap()
         ),
     )
 }
 
-fn print_trench(trench: &[Pos]) {
+fn is_on_vertice(pos: &Pos, v: &(Pos, Pos)) -> bool {
+    assert!(v.0 != v.1);
+    if v.0.col == v.1.col {
+        // Vertical line
+        let (a, b) = if v.0.row < v.1.row { (v.0.row, v.1.row) } else { (v.1.row, v.0.row) };
+        return pos.col == v.0.col && pos.row >= a && pos.row <= b;
+    }
+    if v.0.row == v.1.row {
+        // Horizontal line
+        let (a, b) = if v.0.col < v.1.col { (v.0.col, v.1.col) } else { (v.1.col, v.0.col) };
+        return pos.row == v.0.row && pos.col >= a && pos.col <= b;
+    }
+    false
+}
+
+fn trench_contains(trench: &[(Pos, Pos)], pos: &Pos) -> bool {
+    trench.iter().find(|v| is_on_vertice(pos, v)).is_some()
+}
+
+fn print_trench(trench: &[(Pos, Pos)]) {
     let (min, max) = min_max_of_trench(trench);
     for row in min.row..max.row + 1 {
         for col in min.col..max.col + 1 {
             let p = Pos::new(row, col);
-            // if p == Pos::new(0, 100) {
-            //     print!("\x1b[92m{}\x1b[0m", "X");
-            // } else {
-                print!("{}", if trench.contains(&p) { "#" } else { "." });
-            // }
+            print!("{}", if trench_contains(trench, &p) { "#" } else { "." });
         }
         println!();
     }
 }
 
-struct FillItem {
-    pos: Pos,
-    inside: bool,
-    visited: bool,
+fn vertice_len(v: &(Pos, Pos)) -> u64 {
+    assert!(v.0 != v.1);
+    if v.0.col == v.1.col {
+        return v.0.row.abs_diff(v.1.row)
+    }
+    if v.0.row == v.1.row {
+        return v.0.col.abs_diff(v.1.col)
+    }
+    panic!("Invalid pair for vertice_len(): {:?}", v);
 }
 
-impl FillItem {
-    fn new(pos: Pos) -> Self {
-        Self { pos: pos, inside: false, visited: false }
-    }
-
-    fn inside(pos: Pos) -> Self {
-        Self { pos: pos, inside: false, visited: false }
-    }
+fn trench_len(trench: &[(Pos, Pos)]) -> u64 {
+    trench.iter().map(|pair| vertice_len(pair)).sum()
 }
 
-fn trench_surface(trench: &Vec<Pos>) -> u32 {
-    // Flood-fill approach
-    let start = Pos::new(1, 1);
-    // let start = Pos::new(0, 100);
+/*
+def polygonArea(vertices):
+  #A function to apply the Shoelace algorithm
+  numberOfVertices = len(vertices)
+  sum1 = 0
+  sum2 = 0
+  
+  for i in range(0,numberOfVertices-1):
+    sum1 = sum1 + vertices[i][0] *  vertices[i+1][1]
+    sum2 = sum2 + vertices[i][1] *  vertices[i+1][0]
+  
+  #Add xn.y1
+  sum1 = sum1 + vertices[numberOfVertices-1][0]*vertices[0][1]   
+  #Add x1.yn
+  sum2 = sum2 + vertices[0][0]*vertices[numberOfVertices-1][1]   
+  
+  area = abs(sum1 - sum2) / 2
+  return area
+*/
 
-    let trench_set: HashSet<Pos> = HashSet::from_iter(trench.iter().cloned());
-    let mut filled_trench: HashSet<Pos> = HashSet::new();
+// trench must be on on anticlockwise order
+fn trench_surface(trench: &Vec<(Pos, Pos)>) -> u64 {
+    // Flatten the trench
+    let mut vertices: Vec<Pos> = trench.iter().map(|pair| pair.0.clone()).collect();
 
-    let mut queue: VecDeque<Pos> = VecDeque::new();
-    queue.push_back(start);
+    // anticlockwise
+    vertices.reverse();
+    // println!("{:?}", vertices);
 
-    while !queue.is_empty() {
-        let item = queue.pop_front().unwrap();
-        // assert!(!item.visited);
-
-        // println!("Item {:?}", item);
-        filled_trench.insert(item.clone());
-
-        for n in [item.left(), item.right(), item.up(), item.down()] {
-            if !trench_set.contains(&n) && !filled_trench.contains(&n) {
-                if !queue.contains(&n) {
-                    // println!("  To queue {:?}", n);
-                    queue.push_back(n);
-                }
-            }
-        }
+    // Shoelace algorithm
+    let nb_of_vertices = vertices.len();
+    let mut sum1 = 0;
+    let mut sum2 = 0;
+    for i in 0..nb_of_vertices - 1 {
+        sum1 = sum1 + vertices[i].row * vertices[i+1].col;
+        sum2 = sum2 + vertices[i].col * vertices[i+1].row;
     }
+    sum1 = sum1 + vertices[nb_of_vertices - 1].row * vertices[0].col;
+    let int_area = sum1.abs_diff(sum2) / 2;
 
-    (filled_trench.len() + trench.len()) as u32
+    // Shoelace algorithm
+    // let nb_of_vertices = vertices.len();
+    // let mut area = 0;
+    // let j = nb_of_vertices - 1;
+    // for i in 0..nb_of_vertices {
+    //     area += (vertices[j].row + vertices[i].row) * (vertices[j].col - vertices[i].col);
+    // }
+    // area = area.abs() / 2;
+
+    println!("Interior area {}", int_area);
+    
+    let boundary = trench_len(trench);
+    println!("Boundary {}", boundary);
+
+    // Pick's theorem to add the boundaty, with small adjustment
+    // https://en.wikipedia.org/wiki/Pick%27s_theorem
+    int_area + boundary / 2 + 1
 }
 
 fn build_dig_plan<R>(reader: &mut R) -> Vec<Instruction>
@@ -180,10 +265,20 @@ fn main() {
     let dig_plan = build_dig_plan(&mut stdin.lock());
     // println!("{:?}", dig_plan);
 
-    let trench = dig(&dig_plan);
-    print_trench(&trench);
+    let trench: Vec<(Pos, Pos)> = dig(&dig_plan);
+    // print_trench(&trench);
 
     println!("Part 1: {}", trench_surface(&trench));
+
+    let inverted_plan: Vec<_> = dig_plan.iter().map(|i| i.invert()).collect();
+    // println!("{:?}", inverted_plan);
+
+
+    let trench_inverted = dig(&inverted_plan);
+    // println!("Trench length for inverted instructions: {}", trench_inverted.len());
+    // print_trench(&trench_inverted);
+
+    println!("Part 2: {}", trench_surface(&trench_inverted));
 }
 
 #[cfg(test)]
@@ -192,10 +287,17 @@ pub mod tests {
     use std::{fs::File, io::BufReader};
 
     #[test]
-    fn test_part1() {
+    fn test_part1_and_2() {
         let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
         let dig_plan = build_dig_plan(&mut reader);
         let trench = dig(&dig_plan);
+
+        assert_eq!(trench_len(&trench), 38);
         assert_eq!(trench_surface(&trench), 62);
+
+        let inverted_plan: Vec<_> = dig_plan.iter().map(|i| i.invert()).collect();
+        let trench_inverted = dig(&inverted_plan);
+
+        assert_eq!(trench_surface(&trench_inverted), 952408144115);
     }
 }
