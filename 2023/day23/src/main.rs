f4f59ff4e437cf4b26f136a9b5d8fe415872e459
Vincent Oberle <vincent@oberle.org>
2023-12-23 19:20:39 +0200

    Day 23 part 1: Working

--- 2023/day23/src/main.rs
+++ 2023/day23/src/main.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2023/day/23
 
-use std::io::{self, BufRead};
+use std::{io::{self, BufRead}, collections::{BinaryHeap, HashMap, HashSet}};
 
 // NB: Direction and Grid code is mostly the same as Day 21.
 
@@ -20,6 +20,15 @@ impl Direction {
             East => 1,
             South => 2,
             West => 3,
+        }
+    }
+
+    fn opposite(&self) -> Self {
+        match self {
+            North => South,
+            East => West,
+            South => North,
+            West => East,
         }
     }
 }
@@ -62,6 +71,20 @@ impl Grid {
                     .iter()
                     .collect::<String>()
             );
+        }
+    }
+
+    fn print_with_pos(&self, pos: usize) {
+        for row in 0..self.rows {
+            for p in row * self.cols..(row + 1) * self.cols {
+                let c = self.values[p];
+                if p == pos {
+                    print!("\x1b[91m{}\x1b[0m", c);
+                } else {
+                    print!("{}", c);
+                }
+            };
+            println!();
         }
     }
 
@@ -128,15 +151,166 @@ fn test_grid() {
     assert!(!grid.allowed(5, South));
 }
 
+// Walk until we reach an intersection.
+// Returns the intersection position and the distance to get there.
+// None means we got blocked. Exit of the grid is considered an intersection.
+fn walk(grid: &Grid, from: usize, towards: Direction) -> Option<(usize, u32)> {
+    // if !grid.allowed(from, towards) {
+    //     return None;
+    // }
+    let mut pos = from;
+    let mut opp_dir = towards.opposite();
+    let mut steps = 0;
+    let mut exit = false;
+    loop {
+        // Finds the direction we are allowed to walk
+        let dirs: Vec<Direction> = ALL_DIRECTIONS.iter().filter_map(|&d| {
+            if steps == 0 && d != towards {
+                // On first step, we want to go into a specific direction
+                return None;
+            } else if d == opp_dir {
+                // Not allowed to go back
+                return None;
+            }
+            // println!("{pos} {:?}", d);
+            if let Some(npos) = grid.try_next_pos(pos, d) {
+                // println!("{pos} {:?}, c={}", d, grid.values[npos]);
+                return match grid.values[npos] {
+                    '#' => None, // Not allowed to go into forest
+                    '.' => Some(d), // Paths are ok
+                    '<' => if d == West { Some(d) } else { None },
+                    '>' => if d == East { Some(d) } else { None },
+                    // Observation: ^ is never found in the input.
+                    '^' => if d == North { Some(d) } else { None },
+                    'v' => if d == South { Some(d) } else { None },
+                    _ => panic!("Invalid char in map {}", grid.values[npos]),
+                };
+            } else {
+                if steps > 0 {
+                    // We found the exit!
+                    // println!("Exit found! {}, steps={}", pos, steps);
+                    exit = true;
+                    return Some(d);
+                } else {
+                    return None;
+                }
+            }
+        }).collect();
+
+        if exit {
+            return Some((pos, steps));
+        }
+
+        if dirs.len() == 0 {
+            // dead-end
+            return None;
+        } else if dirs.len() == 1 {
+            // continue walking
+            pos = grid.next_pos(pos, dirs[0]);
+            // grid.print_with_pos(pos);
+            opp_dir = dirs[0].opposite();
+            steps += 1;
+        } else {
+            // intersection
+            // println!("Intersection: steps={}, pos={}, dirs={:?}", steps, pos, dirs);
+            // grid.print_with_pos(pos);
+            return Some((pos, steps));
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Node {
+    pos: usize,
+    cost: u32,
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.cost.partial_cmp(&self.cost)
+    }
+}
+
+
 fn longest_hike_step_count(grid: &Grid) -> u32 {
-    0
+    let start = 1;
+    let end = grid.pos(grid.rows - 1, grid.cols - 2);
+    println!("Start: {}; End: {}", start, end);
+
+    let mut visited: HashSet<usize> = HashSet::new();
+    let mut distance: HashMap<usize, u32> = HashMap::new();
+    
+    let mut queue: BinaryHeap::<Node> = BinaryHeap::new();
+    queue.push(Node { pos: start, cost: 0 });
+
+    let mut answer = u32::MIN;
+
+    while let Some(Node {pos, cost}) = queue.pop() {
+        if pos == end {
+            answer = u32::max(answer, cost);
+            println!("On End {} ({}, {}), cost {}, answer {}", pos, grid.row(pos), grid.col(pos), cost, answer);
+            continue;
+        }
+
+        visited.insert(pos);
+        // println!("Visiting {} ({}, {})", pos, grid.row(pos), grid.col(pos));
+        // grid.print_with_pos(pos);
+
+        queue.extend(ALL_DIRECTIONS.iter().filter_map(|&d| {
+            // TODO optimization to avoid walking back
+            // println!("Walking towards {:?}", d);
+            if let Some((npos, steps)) = walk(grid, pos, d) {
+                // println!(" reached {} after {} steps", npos, steps);
+                let nkey = npos;
+                let ncost = cost + steps;
+                if visited.contains(&nkey) {
+                    return None;
+                }
+                if let Some(prevcost) = distance.get(&nkey) {
+                    if *prevcost > ncost {
+                        return None;
+                    }
+                }
+                distance.insert(nkey, ncost);
+                Some(Node {
+                    pos: npos,
+                    cost: ncost,
+                })
+            } else {
+                return None;
+            }
+        }));
+    }
+
+    println!("{:#?}", distance);
+
+    answer
+    // Get min cost of last tile.
+    // distance.iter()
+    // .filter(|((p, dir), dist)| *p == end)
+    // .map(|((p, dir), dist)| *dist)
+    // // .min_by_key(|((p, dir), dist)| d)
+    // .max()
+    // .unwrap()
+    // history[(tiles.len() - 1) * 4 * MAX..]
+    // .iter()
+    // .map(|(_visited, cost)| *cost)
+    // .min()
+    // .unwrap()
+    // 0
 }
 
 fn main() {
     let stdin = io::stdin();
 
     let grid = Grid::build(&mut stdin.lock());
-    // grid.print();
+    grid.print();
 
     println!("Part 1: {}", longest_hike_step_count(&grid));
 }
@@ -146,11 +320,25 @@ pub mod tests {
     use super::*;
     use std::{fs::File, io::BufReader};
 
+    fn get_grid() -> Grid {
+        let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
+        Grid::build(&mut reader)
+    }
+
+    #[test]
+    fn test_walk() {
+        let grid = get_grid();
+        assert_eq!(walk(&grid, 1, South), Some((grid.pos(5, 3), 15)));
+        assert_eq!(walk(&grid, 1, North), None);
+        assert_eq!(walk(&grid, 1, East), None);
+        assert_eq!(walk(&grid, 1, West), None);
+        // 312
+        assert_eq!(walk(&grid, grid.pos(13, 13), South), Some((grid.pos(22, 21), 25)));
+    }
+
     #[test]
     fn test_part1() {
-        let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
-        let grid = Grid::build(&mut reader);
-
+        let grid = get_grid();
         assert_eq!(longest_hike_step_count(&grid), 94);
     }
 }
