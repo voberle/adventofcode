87ada747069f258bf268800511c42910c008e8be
Vincent Oberle <vincent@oberle.org>
2023-12-24 13:51:30 +0200

    Day 23: Part 1 working

--- 2023/day24/src/main.rs
+++ 2023/day24/src/main.rs
@@ -1,32 +1,6 @@
 // https://adventofcode.com/2023/day/24
 
-/*
-y - y1 = m(x - x1)
-y - y1 = m*x - m*x1
-y - m*x - y1 + m*x1 = 0
-- m*x + y   + (m*x1 - y1) = 0
-m*x - y  + (y1 - m*x1) = 0
-
-
-a1x+b1y+c1=0 and a2x+b2y+c2=0,
-
-(x, y) = ((b1c2-b2c1)/(a1b2-a2b1), (c1a2-c2a1)/(a1b2-a2b1))
-
-Hailstone A: 19, 13, 30 @ -2, 1, -2
-M = 1 / -2 = -0,5
-A1 = M = -0,5
-B1 = -1
-C1 = Y1 - M * X1 = 13 - -0,5*19 = 22.5
-Hailstone B: 18, 19, 22 @ -1, -1, -2
-A2 = -1 / -1 = 1
-B2 = -1
-C2 = 19 -1 * 18 = 1
-
-X = (b1c2-b2c1)/(a1b2-a2b1),
-= ((-1 * 1) - (-1*22.5)) / ( ( -0.5* -1) - (1 * -1) ) 
- */
-
-use std::ops::{Range, RangeInclusive};
+use std::{ops::RangeInclusive, io::{self, BufRead}};
 
 struct Pos {
     x: i64,
@@ -118,71 +92,116 @@ impl Hailstone {
 #[test]
 fn test_intersection() {
     // X and Y position each at least 7 and at most 27
-    let area_x = 7f64..=27f64;
-    let area_y = 7f64..=27f64;
+    let x_area = 7f64..=27f64;
+    let y_area = 7f64..=27f64;
 
     let mut a = Hailstone::new(19, 13, 30, -2, 1, -2);
     let mut b = Hailstone::new(18, 19, 22, -1, -1, -2);
     // Hailstones' paths will cross inside the test area (at x=14.333, y=15.333).
     assert_eq!(a.crosses(&b), Some((14.333, 15.333)));
-    assert!(a.crosses_in_area(&b, &area_x, &area_y));
-    assert!(a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(a.crosses_in_area(&b, &x_area, &y_area));
+    assert!(a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(19, 13, 30, -2, 1, -2);
     b = Hailstone::new(20, 25, 34, -2, -2, -4);
     // Hailstones' paths will cross inside the test area (at x=11.667, y=16.667).
     assert_eq!(a.crosses(&b), Some((11.667, 16.667)));
-    assert!(a.crosses_in_area(&b, &area_x, &area_y));
-    assert!(a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(a.crosses_in_area(&b, &x_area, &y_area));
+    assert!(a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(19, 13, 30, -2, 1, -2);
     b = Hailstone::new(12, 31, 28, -1, -2, -1);
     // Hailstones' paths will cross outside the test area (at x=6.2, y=19.4).
     assert_eq!(a.crosses(&b), Some((6.2, 19.4)));
-    assert!(!a.crosses_in_area(&b, &area_x, &area_y));
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area(&b, &x_area, &y_area));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(19, 13, 30, -2, 1, -2);
     b = Hailstone::new(20, 19, 15, 1, -5, -3);
     // Hailstones' paths crossed in the past for hailstone A.
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(18, 19, 22, -1, -1, -2);
     b = Hailstone::new(20, 25, 34, -2, -2, -4);
     // Hailstones' paths are parallel; they never intersect.
     assert_eq!(a.crosses(&b), None);
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(18, 19, 22, -1, -1, -2);
     b = Hailstone::new(12, 31, 28, -1, -2, -1);
     // Hailstones' paths will cross outside the test area (at x=-6, y=-5).
     assert_eq!(a.crosses(&b), Some((-6.0, -5.0)));
-    assert!(!a.crosses_in_area(&b, &area_x, &area_y));
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area(&b, &x_area, &y_area));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(18, 19, 22, -1, -1, -2);
     b = Hailstone::new(20, 19, 15, 1, -5, -3);
     // Hailstones' paths crossed in the past for both hailstones.
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(20, 25, 34, -2, -2, -4);
     b = Hailstone::new(12, 31, 28, -1, -2, -1);
     // Hailstones' paths will cross outside the test area (at x=-2, y=3).
     assert_eq!(a.crosses(&b), Some((-2.0, 3.0)));
-    assert!(!a.crosses_in_area(&b, &area_x, &area_y));
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area(&b, &x_area, &y_area));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(20, 25, 34, -2, -2, -4);
     b = Hailstone::new(20, 19, 15, 1, -5, -3);
     // Hailstones' paths crossed in the past for hailstone B.
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
 
     a = Hailstone::new(12, 31, 28, -1, -2, -1);
     b = Hailstone::new(20, 19, 15, 1, -5, -3);
     // Hailstones' paths crossed in the past for both hailstones.
-    assert!(!a.crosses_in_area_and_future(&b, &area_x, &area_y));
+    assert!(!a.crosses_in_area_and_future(&b, &x_area, &y_area));
+}
+
+fn count_crossing_hailstones(hailstones: &Vec<Hailstone>, area: &RangeInclusive<f64>) -> i64 {
+    let mut count = 0;
+    for i in 0..hailstones.len() {
+        for j in i+1..hailstones.len() {
+            if hailstones[i].crosses_in_area_and_future(&hailstones[j], &area, &area) {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn build_hailstones<R>(reader: &mut R) -> Vec<Hailstone>
+where
+    R: BufRead,
+{
+    let mut hailstones = Vec::new();
+    for l in reader.lines() {
+        let line = l.unwrap();
+        let pv: Vec<&str> = line.split(" @ ").collect();
+        let pos: Vec<i64> = pv[0].split(", ").map(|v| v.trim().parse().unwrap()).collect();
+        let vel: Vec<i64> = pv[1].split(", ").map(|v| v.trim().parse::<i64>().unwrap()).collect();
+        hailstones.push(Hailstone::new(pos[0], pos[1], pos[2], vel[0], vel[1], vel[2]));
+    }
+    hailstones
 }
 
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let hailstones = build_hailstones(&mut stdin.lock());
+    let area = 200000000000000f64..=400000000000000f64;
+
+    println!("Part 1: {}", count_crossing_hailstones(&hailstones, &area));
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use std::{fs::File, io::BufReader};
+
+    #[test]
+    fn test_part1() {
+        let mut reader = BufReader::new(File::open("resources/input_test").unwrap());
+        let hailstones = build_hailstones(&mut reader);
+        let area = 7f64..=27f64;
+        assert_eq!(count_crossing_hailstones(&hailstones, &area), 2);
+    }
 }
