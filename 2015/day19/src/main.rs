01503dfa238d58f53fe88cb00c50f45b211a8a41
Vincent Oberle <vincent@oberle.org>
2024-01-03 23:51:41 +0200

    2015/Day 19: Brute force for part 2, but doesn't scale

--- 2015/day19/src/main.rs
+++ 2015/day19/src/main.rs
@@ -34,8 +34,41 @@ fn distinct_molecules_count(replacements: &[(String, String)], molecule: &str) -
     set.len()
 }
 
-fn part2(replacements: &[(String, String)], molecule: &str) -> i64 {
-    0
+fn possible_replacements(replacements: &[(String, String)], elt: &str) -> Vec<String> {
+    replacements
+        .iter()
+        .filter(|(s, _)| s == elt)
+        .map(|(_, d)| d)
+        .cloned()
+        .collect()
+}
+
+fn min_steps_for_medicine(replacements: &[(String, String)], molecule: &str) -> usize {
+    let mut steps = 0;
+    // This vector contains all molecules generated on each step
+    let mut generated = vec!["e".to_string()];
+    loop {
+        steps += 1;
+        let mut set: FxHashSet<String> = FxHashSet::default();
+        generated.iter().for_each(|g| {
+            for r in replacements {
+                let source = &r.0;
+                let dest = &r.1;
+                g.match_indices(source).for_each(|(idx, _)| {
+                    let mut new_mol = g.to_string();
+                    new_mol.replace_range(idx..idx + source.len(), dest);
+                    set.insert(new_mol);
+                })
+            }
+        });
+        println!("{steps}: {:?}", set);
+        // println!("{steps}");
+        if set.contains(molecule) {
+            break;
+        }
+        generated = set.into_iter().collect();
+    }
+    steps
 }
 
 fn main() {
@@ -47,24 +80,36 @@ fn main() {
         "Part 1: {}",
         distinct_molecules_count(&replacements, &molecule)
     );
-    println!("Part 2: {}", part2(&replacements, &molecule));
+    println!(
+        "Part 2: {}",
+        min_steps_for_medicine(&replacements, &molecule)
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT_TEST: &str = include_str!("../resources/input_test_1");
+    const INPUT_TEST_1: &str = include_str!("../resources/input_test_1");
 
     #[test]
     fn test_part1() {
-        let (replacements, molecule) = build(INPUT_TEST);
+        let (replacements, molecule) = build(INPUT_TEST_1);
         assert_eq!(distinct_molecules_count(&replacements, &molecule), 4);
     }
 
+    const INPUT_TEST_2: &str = include_str!("../resources/input_test_2");
+    const INPUT_TEST_3: &str = include_str!("../resources/input_test_3");
+
     #[test]
-    fn test_part2() {
-        let (replacements, molecule) = build(INPUT_TEST);
-        assert_eq!(part2(&replacements, &molecule), 0);
+    fn test_part2_1() {
+        let (replacements, molecule) = build(INPUT_TEST_2);
+        assert_eq!(min_steps_for_medicine(&replacements, &molecule), 3);
+    }
+
+    #[test]
+    fn test_part2_2() {
+        let (replacements, molecule) = build(INPUT_TEST_3);
+        assert_eq!(min_steps_for_medicine(&replacements, &molecule), 6);
     }
 }
