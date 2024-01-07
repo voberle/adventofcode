4a4a2b2ab9a7aa5d03b9967a2435f52338636fce
Vincent Oberle <vincent@oberle.org>
2024-01-07 18:51:19 +0200

    Template: Add build function

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,18 +1,26 @@
 use std::io::{self, Read};
 
-fn part1(input: &str) -> i64 {
+type Input = str;
+
+fn build(input: &str) -> String {
+    input.to_string()
+}
+
+fn part1(input: &Input) -> i64 {
     0
 }
 
-fn part2(input: &str) -> i64 {
+fn part2(input: &Input) -> i64 {
     0
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    let input_parsed = build(&input);
+
+    println!("Part 1: {}", part1(&input_parsed));
+    println!("Part 2: {}", part2(&input_parsed));
 }
 
 #[cfg(test)]
@@ -23,11 +31,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT_TEST), 0);
+        assert_eq!(part1(&build(INPUT_TEST)), 0);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT_TEST), 0);
+        assert_eq!(part2(&build(INPUT_TEST)), 0);
     }
 }
