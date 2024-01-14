e053172b7b734978053673f18c8f01fbec5900f2
Vincent Oberle <vincent@oberle.org>
2024-01-14 19:38:53 +0200

    2016/Day 20: Simplification of Part 1

--- 2016/day20/src/main.rs
+++ 2016/day20/src/main.rs
@@ -1,80 +1,22 @@
-use core::fmt::Debug;
 use std::{
     cmp::{Ordering, PartialOrd},
-    fmt::{self, Display},
     io::{self, Read},
 };
 
-/// Range.
-/// start is inclusive, end exclusive.
-///
-/// Using our own range implementation, as Rust's one isn't very powerfull
-/// https://kaylynn.gay/blog/post/rust_ranges_and_suffering
-#[derive(Debug)]
-struct Range<T>
-where
-    T: PartialOrd + Copy + Default + Display,
-{
-    start: T,
-    end: T,
-}
-
-impl<T> Range<T>
-where
-    T: PartialOrd + Copy + Default + Display,
-{
-    fn new(start: T, end: T) -> Self {
-        assert!(start < end);
-        Self { start, end }
-    }
-
-    fn contains(&self, val: T) -> bool {
-        self.start <= val && val <= self.end
-    }
-
-    /// Check if self is subset of other
-    fn is_subset_of(&self, other: &Range<T>) -> bool {
-        self.start >= other.start && self.end <= other.end
-    }
-}
-
-// Conversion from Rust ranges.
-impl<T> From<std::ops::Range<T>> for Range<T>
-where
-    T: PartialOrd + Copy + Default + Display,
-{
-    fn from(item: std::ops::Range<T>) -> Self {
-        Range::new(item.start, item.end)
-    }
-}
-
-// impl<T> From<std::ops::RangeInclusive<T>> for Range<T> where T: PartialOrd + Copy + Default + Display + std::ops::Add<T, Output = T> {
-//     fn from(item: std::ops::RangeInclusive<T>) -> Self {
-//         Range::new(*item.start(), *item.end() + T::from(1))
-//     }
-// }
-
-impl<T> fmt::Display for Range<T>
-where
-    T: PartialOrd + Copy + Default + Display,
-{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}-{}", self.start, self.end)
-    }
-}
-
 /// Takes a list of ranges and simplifies it into an ordered non-overlapping list.
-fn simplify_ranges<T>(ranges: &[Range<T>]) -> Vec<Range<T>>
+/// Works only with ranges that have inclusive start and exclusive end.
+fn simplify_ranges<T>(ranges: &[(T, T)]) -> Vec<(T, T)>
 where
-    T: PartialOrd + Copy + Default + Debug + Display,
+    T: PartialOrd + Copy,
 {
     // Algorith from https://cs.stackexchange.com/a/106978
-    // Works only with ranges that have inclusive start and exclusive end.
     // List of beginning and ending points. The boolean tells the type (true is start, false is end).
     let mut positions: Vec<(bool, T)> = ranges
         .iter()
-        .flat_map(|r| [(true, r.start), (false, r.end)])
+        .flat_map(|r| [(true, r.0), (false, r.1)])
         .collect();
+
+    // Sort by position, with starting points comparing below ending points when positions are equal.
     positions.sort_by(|a, b| {
         if a.1 == b.1 {
             if a.0 && !b.0 {
@@ -90,8 +32,10 @@ where
             a.1.partial_cmp(&b.1).unwrap()
         }
     });
-    let mut simplified_ranges: Vec<Range<T>> = Vec::new();
-    let mut start: T = T::default();
+    assert!(positions.len() > 1 && positions.first().unwrap().0);
+
+    let mut simplified_ranges = Vec::new();
+    let mut start: T = positions.first().unwrap().1; // we need a value to initialize start with.
     let mut c = 0;
     for (pos_type, pos) in positions {
         if pos_type {
@@ -103,7 +47,8 @@ where
         } else {
             // 1->0 transition
             if c == 1 {
-                simplified_ranges.push(Range::new(start, pos));
+                let end = pos;
+                simplified_ranges.push((start, end));
             }
             c -= 1;
         }
@@ -111,26 +56,28 @@ where
     simplified_ranges
 }
 
-fn build(input: &str) -> Vec<Range<u64>> {
+fn build(input: &str) -> Vec<(u32, u32)> {
     input
         .lines()
         .map(|line| {
-            let parts: Vec<u64> = line.split('-').map(|v| v.parse().unwrap()).collect();
-            Range::<u64>::from(parts[0]..parts[1] + 1)
+            let parts: Vec<u32> = line.split('-').map(|v| v.parse().unwrap()).collect();
+            (parts[0], parts[1])
         })
         .collect()
 }
 
-fn lowest_allowed_ip(blocked_ips: &[Range<u64>]) -> u64 {
-    let simplified = simplify_ranges(blocked_ips);
-    // println!("Range count: {}", simplified.len());
-    // for r in simplified {
-    //     println!("{}", r);
-    // }
-    simplified.first().expect("No ranges found").end
+fn lowest_allowed_ip(blocked_ips: &[(u32, u32)]) -> u32 {
+    // We need exclusive ranges for the simplify method, so we need to use u64 as u32 + 1 may overflow.
+    let ranges_excl: Vec<(u64, u64)> = blocked_ips
+        .iter()
+        .map(|r| (r.0 as u64, r.1 as u64 + 1))
+        .collect();
+
+    let simplified = simplify_ranges(&ranges_excl);
+    simplified.first().expect("No ranges found").1 as u32
 }
 
-fn part2(blocked_ips: &[Range<u64>]) -> u64 {
+fn part2(blocked_ips: &[(u32, u32)]) -> u32 {
     0
 }
 
@@ -148,6 +95,12 @@ mod tests {
     use super::*;
 
     const INPUT_TEST: &str = include_str!("../resources/input_test_1");
+
+    #[test]
+    fn test_simplify_ranges() {
+        let ranges = [(5, 9), (0, 3), (4, 8)];
+        assert_eq!(simplify_ranges(&ranges), &[(0, 3), (4, 9)]);
+    }
 
     #[test]
     fn test_part1() {
