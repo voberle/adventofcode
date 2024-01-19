ca3a512b7bf72e4472f33a06281dd84281282278
Vincent Oberle <vincent@oberle.org>
2024-01-19 22:13:09 +0200

    AoC utils: Use rem_euclid() for wrapping methods

--- utils/src/array.rs
+++ utils/src/array.rs
@@ -1,15 +1,12 @@
 /// To get the last element of an array with index -1, or the first with index len.
 #[allow(clippy::cast_possible_truncation)]
 #[allow(clippy::cast_possible_wrap)]
-#[allow(clippy::cast_sign_loss)]
 const fn wrapping(i: i32, len: usize) -> usize {
-    // https://stackoverflow.com/a/45397704
-    let c = len as i32;
-    ((i % c + c) % c) as usize
+    i.rem_euclid(len as i32) as usize
 }
 
 const fn wrapping_index(i: usize, len: usize) -> usize {
-    (i % len + len) % len
+    i.rem_euclid(len)
 }
 
 #[test]
@@ -18,4 +15,8 @@ fn test_wrapping_index() {
     assert_eq!(wrapping(0, 6), 0);
     assert_eq!(wrapping(3, 6), 3);
     assert_eq!(wrapping(6, 6), 0);
+
+    assert_eq!(wrapping_index(0, 6), 0);
+    assert_eq!(wrapping_index(3, 6), 3);
+    assert_eq!(wrapping_index(6, 6), 0);
 }
