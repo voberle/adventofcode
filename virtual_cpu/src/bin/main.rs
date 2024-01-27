905fc97a8d3e27f8650108151bc93908fe49750d
Vincent Oberle <vincent@oberle.org>
2024-01-27 22:36:06 +0200

    Virtual CPU: Improve main.rs, separate its support code

--- virtual_cpu/src/bin/main.rs
+++ virtual_cpu/src/bin/main.rs
@@ -1,3 +1,5 @@
+mod utils;
+
 mod day2015_23;
 mod day2016_12;
 mod day2016_23;
@@ -5,89 +7,43 @@ mod day2016_25;
 mod day2017_18;
 mod day2017_23;
 
-use std::fs::{self, File};
-use std::io::{BufRead, BufReader};
-
-use fxhash::FxHashMap;
-
+use utils::*;
 use virtual_cpu::c_code::exec::exec_c_code;
-use virtual_cpu::test_utils;
-
-// Loads the list of puzzle answers, saving it into a map "puzzle name" => "answer".
-fn load_answer_list() -> FxHashMap<String, String> {
-    let reader =
-        BufReader::new(File::open(format!("{}/answers", test_utils::RESOURCES_DIR)).unwrap());
-    reader
-        .lines()
-        .map(|line| {
-            let l = line.unwrap();
-            let p: Vec<_> = l.split(' ').collect();
-            (p[0].to_string(), p[1].to_string())
-        })
-        .collect()
-}
-
-fn parse_puzzle_name(s: &str) -> (u32, u32) {
-    let p: Vec<_> = s.strip_prefix("day").unwrap().split('_').collect();
-    (p[0].parse().unwrap(), p[1].parse().unwrap())
-}
-
-type PuzzleFn = fn(&str) -> String;
-type GetCCodeFn = fn(&str) -> String;
-
-#[rustfmt::skip]
-const PUZZLES: [(&str, u8, PuzzleFn, Option<GetCCodeFn>); 8] = [
-    ("day2015_23", 1, day2015_23::part1, Some(day2015_23::part1_c_code)),
-    ("day2015_23", 2, day2015_23::part2, Some(day2015_23::part2_c_code)),
-    ("day2016_12", 1, day2016_12::part1, None),
-    ("day2016_12", 2, day2016_12::part2, None),
-    ("day2016_23", 1, day2016_23::part1, None),
-    ("day2016_25", 1, day2016_25::part1, None),
-    ("day2017_18", 1, day2017_18::part1, None),
-    ("day2017_23", 1, day2017_23::part1, None),
-];
 
 fn main() {
-    const USE_C_CONVERSION: bool = false;
+    #[rustfmt::skip]
+    let puzzles = vec![
+        Puzzle::both("day2015_23", 1, day2015_23::part1, day2015_23::part1_c_code),
+        Puzzle::both("day2015_23", 2, day2015_23::part2, day2015_23::part2_c_code),
+        Puzzle::base("day2016_12", 1, day2016_12::part1),
+        Puzzle::base("day2016_12", 2, day2016_12::part2),
+        Puzzle::base("day2016_23", 1, day2016_23::part1),
+        Puzzle::base("day2016_25", 1, day2016_25::part1),
+        Puzzle::base("day2017_18", 1, day2017_18::part1),
+        Puzzle::base("day2017_23", 1, day2017_23::part1),
+    ];
 
     let answers = load_answer_list();
 
-    for (puzzle_name, part_nb, puzzle_fn, c_code_fn_option) in PUZZLES {
+    for puzzle in puzzles {
         let expected_result = answers
-            .get(&format!("{}_{}", puzzle_name, part_nb))
-            .unwrap_or_else(|| panic!("Missing answer for {} part {}", puzzle_name, part_nb));
+            .get(&format!("{}_{}", puzzle.name, puzzle.part_nb))
+            .unwrap_or_else(|| {
+                panic!("Missing answer for {} part {}", puzzle.name, puzzle.part_nb)
+            });
 
-        let input_file = test_utils::get_input_file(puzzle_name);
-        let input = fs::read_to_string(input_file).expect("Unable to read input file");
+        let input = puzzle.get_input();
 
-        let res = if USE_C_CONVERSION {
-            if let Some(c_code_fn) = c_code_fn_option {
-                let code = c_code_fn(&input);
-                // println!("{}", code);
-                exec_c_code(&code)
-            } else {
-                continue;
-            }
-        } else {
-            puzzle_fn(&input)
-        };
-
-        // println!("{}: {}", puzzle_name, res);
-
-        let (year, day) = parse_puzzle_name(puzzle_name);
-        println!(
-            "{} {} day {}, part {}",
-            if res == *expected_result {
-                "✅"
-            } else {
-                "❌"
-            },
-            year,
-            day,
-            part_nb
-        );
-        if res != *expected_result {
-            println!("Incorrect result is {}, expected {}", res, expected_result);
+        if let Some(c_code_fn) = puzzle.c_code_fn {
+            let code = c_code_fn(&input);
+            // println!("{}", code);
+            let res = exec_c_code(&code);
+            print_result(&puzzle, &res, expected_result, true);
         }
+
+        if let Some(puzzle_fn) = puzzle.puzzle_fn {
+            let res = puzzle_fn(&input);
+            print_result(&puzzle, &res, expected_result, false);
+        };
     }
 }
---
+++ virtual_cpu/src/bin/utils.rs
@@ -0,0 +1,87 @@
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader};
+
+use fxhash::FxHashMap;
+
+use virtual_cpu::test_utils;
+
+// Loads the list of puzzle answers, saving it into a map "puzzle name" => "answer".
+pub fn load_answer_list() -> FxHashMap<String, String> {
+    let reader =
+        BufReader::new(File::open(format!("{}/answers", test_utils::RESOURCES_DIR)).unwrap());
+    reader
+        .lines()
+        .map(|line| {
+            let l = line.unwrap();
+            let p: Vec<_> = l.split(' ').collect();
+            (p[0].to_string(), p[1].to_string())
+        })
+        .collect()
+}
+
+type PuzzleFn = fn(&str) -> String;
+type GetCCodeFn = fn(&str) -> String;
+
+pub struct Puzzle {
+    pub name: String,
+    pub year: u32,
+    pub day: u32,
+    pub part_nb: u8,
+    pub puzzle_fn: Option<PuzzleFn>,
+    pub c_code_fn: Option<GetCCodeFn>,
+}
+
+impl Puzzle {
+    fn new(
+        name: &str,
+        part_nb: u8,
+        puzzle_fn: Option<PuzzleFn>,
+        c_code_fn: Option<GetCCodeFn>,
+    ) -> Self {
+        let (year, day) = Self::parse_puzzle_name(name);
+        Self {
+            name: name.to_string(),
+            year,
+            day,
+            part_nb,
+            puzzle_fn,
+            c_code_fn,
+        }
+    }
+
+    pub fn both(name: &str, part_nb: u8, puzzle_fn: PuzzleFn, c_code_fn: GetCCodeFn) -> Self {
+        Self::new(name, part_nb, Some(puzzle_fn), Some(c_code_fn))
+    }
+
+    pub fn base(name: &str, part_nb: u8, puzzle_fn: PuzzleFn) -> Self {
+        Self::new(name, part_nb, Some(puzzle_fn), None)
+    }
+
+    pub fn with_c(name: &str, part_nb: u8, c_code_fn: GetCCodeFn) -> Self {
+        Self::new(name, part_nb, None, Some(c_code_fn))
+    }
+
+    pub fn parse_puzzle_name(s: &str) -> (u32, u32) {
+        let p: Vec<_> = s.strip_prefix("day").unwrap().split('_').collect();
+        (p[0].parse().unwrap(), p[1].parse().unwrap())
+    }
+
+    pub fn get_input(&self) -> String {
+        let input_file = test_utils::get_input_file(&self.name);
+        fs::read_to_string(input_file).expect("Unable to read input file")
+    }
+}
+
+pub fn print_result(puzzle: &Puzzle, res: &str, expected: &str, is_c: bool) {
+    println!(
+        "{} {} day {}, part {}{}",
+        if res == expected { "✅" } else { "❌" },
+        puzzle.year,
+        puzzle.day,
+        puzzle.part_nb,
+        if is_c { " C version" } else { "" },
+    );
+    if res != expected {
+        println!("Incorrect result is {}, expected {}", res, expected);
+    }
+}
