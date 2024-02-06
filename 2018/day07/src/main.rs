e7660a3917e00d76cfb61cc6ff057ab05079d856
Vincent Oberle <vincent@oberle.org>
2024-02-06 13:45:02 +0200

    2018/Day 07: Refactoring to prepare part 2

--- 2018/day07/src/main.rs
+++ 2018/day07/src/main.rs
@@ -29,45 +29,41 @@ fn idx2char(idx: usize) -> char {
     char::from(b'A' + idx as u8)
 }
 
-fn steps_in_order<const STEP_COUNT: usize>(deps: &[Vec<usize>]) -> String {
-    // Since we just deal with up to 26 steps (letters in alphabetical order),
-    // tracking them in an array is convenient.
-    //  0: Not ready to exec.
-    //  1-26: Executed, in that order.
-    //  READY: Not executed, but ready.
-    const NOT_READY: usize = 0;
-    const READY: usize = usize::MAX;
-    let mut letters = [READY; STEP_COUNT];
+const NOT_READY: usize = 0;
+const READY: usize = usize::MAX;
 
+// Since we just deal with up to 26 steps (letters in alphabetical order),
+// tracking them in an array is convenient.
+//  0: Not ready to exec.
+//  1-26: Executed, in that order.
+//  READY: Not executed, but ready.
+fn build_steps_array<const STEP_COUNT: usize>(deps: &[Vec<usize>]) -> [usize; STEP_COUNT] {
+    let mut letters = [READY; STEP_COUNT];
     // Mark all that cannot be executed initially.
     for (i, d) in deps.iter().enumerate() {
         if !d.is_empty() {
             letters[i] = NOT_READY;
         }
     }
+    letters
+}
 
-    let mut pos = 1;
-    while pos <= STEP_COUNT {
-        // Mark the steps ready to be executed.
-        for idx in 0..STEP_COUNT {
-            if letters[idx] == NOT_READY {
-                // If it's on the right side of a dependency and not ready, we can only do it if we have done all the prerequisites.
-                if deps[idx]
-                    .iter()
-                    .all(|v| (1..=STEP_COUNT).contains(&letters[*v]))
-                {
-                    letters[idx] = READY;
-                }
+// Mark the steps ready to be executed.
+fn mark_ready<const STEP_COUNT: usize>(deps: &[Vec<usize>], letters: &mut [usize]) {
+    for idx in 0..STEP_COUNT {
+        if letters[idx] == NOT_READY {
+            // If it's on the right side of a dependency and not ready, we can only do it if we have done all the prerequisites.
+            if deps[idx]
+                .iter()
+                .all(|v| (1..=STEP_COUNT).contains(&letters[*v]))
+            {
+                letters[idx] = READY;
             }
         }
-
-        // Do first in alphabetical order.
-        if let Some(to_exec_idx) = letters.iter().position(|v| *v == READY) {
-            letters[to_exec_idx] = pos;
-            pos += 1;
-        }
     }
+}
 
+fn steps_array_to_string<const STEP_COUNT: usize>(letters: &[usize]) -> String {
     (1..=STEP_COUNT)
         .map(|idx| {
             let c_as_int = letters.iter().position(|i| *i == idx).unwrap();
@@ -76,7 +72,24 @@ fn steps_in_order<const STEP_COUNT: usize>(deps: &[Vec<usize>]) -> String {
         .collect()
 }
 
-fn part2(deps: &[Vec<usize>]) -> i64 {
+fn steps_in_order<const STEP_COUNT: usize>(deps: &[Vec<usize>]) -> String {
+    let mut letters: [usize; STEP_COUNT] = build_steps_array(deps);
+
+    let mut pos = 1;
+    while pos <= STEP_COUNT {
+        mark_ready::<STEP_COUNT>(deps, &mut letters);
+
+        // Do first in alphabetical order.
+        if let Some(to_exec_idx) = letters.iter().position(|v| *v == READY) {
+            letters[to_exec_idx] = pos;
+            pos += 1;
+        }
+    }
+
+    steps_array_to_string::<STEP_COUNT>(&letters)
+}
+
+fn time_to_complete(deps: &[Vec<usize>], workers: usize, step_duration_offset: usize) -> usize {
     0
 }
 
@@ -86,7 +99,7 @@ fn main() {
     let dependencies = build::<26>(input.trim());
 
     println!("Part 1: {}", steps_in_order::<26>(&dependencies));
-    println!("Part 2: {}", part2(&dependencies));
+    println!("Part 2: {}", time_to_complete(&dependencies, 5, 60));
 }
 
 #[cfg(test)]
@@ -102,6 +115,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&build::<6>(INPUT_TEST)), 0);
+        assert_eq!(time_to_complete(&build::<6>(INPUT_TEST), 2, 0), 0);
     }
 }
