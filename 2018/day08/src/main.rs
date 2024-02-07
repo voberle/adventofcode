5635c3335c45388e4f87cffd1e59a8a163b081e2
Vincent Oberle <vincent@oberle.org>
2024-02-07 17:59:51 +0200

    2018/Day 08: Minor improvements

--- 2018/day08/src/main.rs
+++ 2018/day08/src/main.rs
@@ -5,7 +5,7 @@ use std::{
 
 #[derive(Debug)]
 struct Node {
-    child_nodes: Vec<Node>,
+    children: Vec<Node>,
     metadata: Vec<u32>,
 }
 
@@ -18,35 +18,39 @@ impl Node {
             .map(|_| numbers.pop_front().unwrap())
             .collect();
         Self {
-            child_nodes,
+            children: child_nodes,
             metadata,
         }
     }
+
+    fn metadata_sum(&self) -> u32 {
+        self.metadata.iter().sum()
+    }
 }
 
+// Here is a neat solution with iterators
+// https://www.reddit.com/r/adventofcode/comments/a47ubw/comment/ebc7orl/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 fn build_tree(input: &str) -> Node {
     // Using a VecDeque as we will pop items from front.
     let mut numbers: VecDeque<u32> = input.split(' ').map(|i| i.parse().unwrap()).collect();
     Node::new(&mut numbers)
 }
 
-fn metadata_sum(node: &Node) -> u32 {
-    node.metadata.iter().sum::<u32>() + node.child_nodes.iter().map(metadata_sum).sum::<u32>()
+fn all_metadata_sum(node: &Node) -> u32 {
+    node.metadata_sum() + node.children.iter().map(all_metadata_sum).sum::<u32>()
 }
 
 fn node_value(node: &Node) -> u32 {
-    if node.child_nodes.is_empty() {
-        node.metadata.iter().sum::<u32>()
+    if node.children.is_empty() {
+        node.metadata_sum()
     } else {
         node.metadata
             .iter()
             .map(|m| {
-                let m_idx = (*m - 1) as usize;
-                if m_idx < node.child_nodes.len() {
-                    node_value(&node.child_nodes[m_idx])
-                } else {
-                    0
-                }
+                node.children
+                    .get(*m as usize - 1)
+                    .map(node_value)
+                    .unwrap_or_default()
             })
             .sum::<u32>()
     }
@@ -58,7 +62,7 @@ fn main() {
     let root = build_tree(input.trim());
     // println!("{:?}", root);
 
-    println!("Part 1: {}", metadata_sum(&root));
+    println!("Part 1: {}", all_metadata_sum(&root));
     println!("Part 2: {}", node_value(&root));
 }
 
@@ -70,7 +74,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(metadata_sum(&build_tree(INPUT_TEST)), 138);
+        assert_eq!(all_metadata_sum(&build_tree(INPUT_TEST)), 138);
     }
 
     #[test]
