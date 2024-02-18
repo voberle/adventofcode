63bd8d6bd97253bfb84dc0980ef83841caa4ea84
Vincent Oberle <vincent@oberle.org>
2024-02-18 18:20:47 +0200

    2018/Day 21: C version: Optimization

--- 2018/day21/resources/main.c
+++ 2018/day21/resources/main.c
@@ -3,31 +3,32 @@
 
 int main(int argc, char *argv[]) {
     long long r0 = atoi(argv[1]);
-    printf("r0 %lli\n", r0);
 
     long long r1 = 0;
     long long r4 = 0;
 
-line6:
-    r4 = r1 | 0x10000;
-    r1 = 16298264;
-line8:
-    r1 = r1 + (r4 & 0xFF);
-    r1 = r1 & 0xFFFFFF;
-    r1 = r1 * 65899;
-    r1 = r1 & 0xFFFFFF;
+    while (1) {
+        r4 = r1 | 0x10000;
+        r1 = 16298264;
+    line8:
+        r1 = r1 + (r4 & 0xFF);
+        r1 = r1 & 0xFFFFFF;
+        r1 = r1 * 65899;
+        r1 = r1 & 0xFFFFFF;
 
-    if (r4 < 256) {
-        goto line28;
+        if (r4 < 256) {
+            goto line28;
+        }
+
+        r4 = r4 / 256;
+        goto line8;
+
+    line28:
+        if (r1 == r0) {
+            break;
+        }
     }
-
-    r4 = r4 / 256;
-    goto line8;
-
-line28:
-    if (r1 != r0) {
-        goto line6;
-    }
+    printf("r0 %lli r1 %lli r4 %lli\n", r0, r1, r4);
 
     return 0;
 }
