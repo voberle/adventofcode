4889706890e7d472d4679baf432ca9bd501d2f2b
Vincent Oberle <vincent@oberle.org>
2024-04-14 18:10:57 +0300

    2020/Day 14: Remove dupe code

--- 2020/day14/src/main.rs
+++ 2020/day14/src/main.rs
@@ -88,7 +88,40 @@ fn build(input: &str) -> Vec<Instruction> {
         .collect()
 }
 
-fn memory_sum_v1(instructions: &[Instruction]) -> u64 {
+type WriteMemFn = fn(&mut FxHashMap<u64, u64>, &Bitmask, u64, u64);
+
+fn write_mem_v1(memory: &mut FxHashMap<u64, u64>, mask: &Bitmask, address: u64, value: u64) {
+    memory.insert(address, mask.apply_to_value(value));
+}
+
+fn write_mem_v2(memory: &mut FxHashMap<u64, u64>, mask: &Bitmask, address: u64, value: u64) {
+    let masked_address = mask.apply_to_address(address);
+
+    // Get how many Xs are in the masked address and how many different addresses this makes.
+    // Our input doesn't have many Xs in each address, so this approach works.
+    let x_count = masked_address.count_x();
+    let addresses_count = 2_u64.pow(u32::try_from(x_count).unwrap());
+
+    for i in 0..addresses_count {
+        // Replace the Xs.
+        let mut i_as_str: String = (0..x_count)
+            .map(|n| if (i >> n) & 1 == 1 { '1' } else { '0' })
+            .collect();
+        let bstr: String = masked_address
+            .0
+            .chars()
+            .map(|v| if v == 'X' { i_as_str.pop().unwrap() } else { v })
+            .collect();
+
+        // Convert to int.
+        let a = u64::from_str_radix(&bstr, 2).expect("Not a binary number!");
+
+        // Add to memory.
+        memory.insert(a, value);
+    }
+}
+
+fn memory_sum(instructions: &[Instruction], write_mem_fn: WriteMemFn) -> u64 {
     let mut memory: FxHashMap<u64, u64> = FxHashMap::default();
     // We want bitmask to be a reference to the mask stored in the instructions, and not make a copy.
     // So we need to declare it here, and initialize it later.
@@ -100,46 +133,7 @@ fn memory_sum_v1(instructions: &[Instruction]) -> u64 {
             }
             Instruction::Memory(address, value) => {
                 let mask = unsafe { std::mem::MaybeUninit::assume_init(bitmask) };
-                memory.insert(*address, mask.apply_to_value(*value));
-            }
-        }
-    }
-    memory.values().sum()
-}
-
-fn memory_sum_v2(instructions: &[Instruction]) -> u64 {
-    let mut memory: FxHashMap<u64, u64> = FxHashMap::default();
-    let mut bitmask = MaybeUninit::<&Bitmask>::uninit();
-    for ins in instructions {
-        match ins {
-            Instruction::Mask(mask) => {
-                bitmask.write(mask);
-            }
-            Instruction::Memory(address, value) => {
-                let mask = unsafe { std::mem::MaybeUninit::assume_init(bitmask) };
-                let masked_address = mask.apply_to_address(*address);
-
-                // Get how many Xs are in the masked address and how many different addresses this makes.
-                let x_count = masked_address.count_x();
-                let addresses_count = 2_u64.pow(u32::try_from(x_count).unwrap());
-
-                for i in 0..addresses_count {
-                    // Replace the Xs.
-                    let mut i_as_str: String = (0..x_count)
-                        .map(|n| if (i >> n) & 1 == 1 { '1' } else { '0' })
-                        .collect();
-                    let bstr: String = masked_address
-                        .0
-                        .chars()
-                        .map(|v| if v == 'X' { i_as_str.pop().unwrap() } else { v })
-                        .collect();
-
-                    // Convert to int.
-                    let a = u64::from_str_radix(&bstr, 2).expect("Not a binary number!");
-
-                    // Add to memory.
-                    memory.insert(a, *value);
-                }
+                write_mem_fn(&mut memory, mask, *address, *value);
             }
         }
     }
@@ -151,8 +145,8 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let instructions = build(&input);
 
-    println!("Part 1: {}", memory_sum_v1(&instructions));
-    println!("Part 2: {}", memory_sum_v2(&instructions));
+    println!("Part 1: {}", memory_sum(&instructions, write_mem_v1));
+    println!("Part 2: {}", memory_sum(&instructions, write_mem_v2));
 }
 
 #[cfg(test)]
@@ -171,11 +165,11 @@ mod tests {
     }
     #[test]
     fn test_part1() {
-        assert_eq!(memory_sum_v1(&build(INPUT_TEST_1)), 165);
+        assert_eq!(memory_sum(&build(INPUT_TEST_1), write_mem_v1), 165);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(memory_sum_v2(&build(INPUT_TEST_2)), 208);
+        assert_eq!(memory_sum(&build(INPUT_TEST_2), write_mem_v2), 208);
     }
 }
