77ac969e323ad45c532cd8637adaaa2b9cff630e
Vincent Oberle <vincent@oberle.org>
2024-05-27 15:54:12 +0300

    2022/Day 01: Optimized version

--- 2022/day01/README.md
+++ 2022/day01/README.md
@@ -7,3 +7,19 @@ What is the most efficient and most elegant to parse such an input of blocks of 
 ## Part 2
 
 Simple stuff.
+
+## Performance
+
+I optimized the solution to build immediately a vector of sums, and sort it. That runs in *38 µs* instead of *65 µs*.
+
+I also tried a version with `BinaryHeap` (which avoids having to sort the vector), but it's slower, *68 µs*.
+
+A shorter parsing approach like the following is also slower, *55 µs*:
+
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|food| food.parse::<u32>().unwrap_or(0)).sum()
+        })
+        .sorted().rev().collect()
--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,41 +1,36 @@
 use std::io::{self, Read};
 
-use itertools::Itertools;
-
-fn build(input: &str) -> Vec<Vec<u32>> {
+// Return the total calories per elf, sorted in ascending order.
+fn build_elf_calories(input: &str) -> Vec<u32> {
     // Is there a better way to parse the input?
     let mut all_calories = Vec::new();
-    let mut elf_calories = Vec::new();
+    let mut elf_calories = 0;
     for line in input.lines() {
         if line.is_empty() {
             all_calories.push(elf_calories);
-            elf_calories = Vec::new();
+            elf_calories = 0;
             continue;
         }
-        elf_calories.push(line.parse().unwrap());
+        elf_calories += line.parse::<u32>().unwrap();
     }
     all_calories.push(elf_calories);
+
+    all_calories.sort_unstable();
     all_calories
 }
 
-fn most_calories(calories: &[Vec<u32>]) -> u32 {
-    calories.iter().map(|e| e.iter().sum()).max().unwrap()
+fn most_calories(calories: &[u32]) -> u32 {
+    *calories.last().unwrap()
 }
 
-fn top_3_calories_sum(calories: &[Vec<u32>]) -> u32 {
-    calories
-        .iter()
-        .map(|e| e.iter().sum::<u32>())
-        .sorted_unstable()
-        .rev()
-        .take(3)
-        .sum()
+fn top_3_calories_sum(calories: &[u32]) -> u32 {
+    calories[calories.len() - 3..].iter().sum()
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let calories = build(&input);
+    let calories = build_elf_calories(&input);
 
     println!("Part 1: {}", most_calories(&calories));
     println!("Part 2: {}", top_3_calories_sum(&calories));
@@ -49,11 +44,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(most_calories(&build(INPUT_TEST)), 24000);
+        assert_eq!(most_calories(&build_elf_calories(INPUT_TEST)), 24000);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(top_3_calories_sum(&build(INPUT_TEST)), 45000);
+        assert_eq!(top_3_calories_sum(&build_elf_calories(INPUT_TEST)), 45000);
     }
 }
