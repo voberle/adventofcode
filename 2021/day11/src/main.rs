abe46bac8b5e92e54dfa5155f2c6363e20c14cc1
Vincent Oberle <vincent@oberle.org>
2024-06-05 17:43:29 +0300

    2021/Day 11: Fix typo in adjacent finding

--- 2021/day11/src/main.rs
+++ 2021/day11/src/main.rs
@@ -61,7 +61,7 @@ impl Grid {
                 ((pos % self.cols) as isize + d_col) as usize,
             )
         })
-        .filter(|&(row, col)| (row < self.cols && col < self.rows))
+        .filter(|&(row, col)| (row < self.rows && col < self.cols))
         .map(|(row, col)| row * self.cols + col)
         .collect()
     }
