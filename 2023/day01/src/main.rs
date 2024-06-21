487a6b79483fb4c103b2e28e2f78e8e1c08dbede
Vincent Oberle <vincent@oberle.org>
2024-06-21 18:37:05 +0300

    2023/Day 01: Switch to iterator

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -5,21 +5,25 @@ fn build(input: &str) -> Vec<&str> {
 }
 
 fn calibration_values_sum_v1(calibration_values: &[&str]) -> u32 {
-    let mut total: u32 = 0;
-    for value in calibration_values {
-        let d: Vec<u32> = value.chars().filter_map(|c| c.to_digit(10)).collect();
-        if !d.is_empty() {
-            let d1 = d.first().unwrap();
-            let d2 = d.last().unwrap();
-            let line_total = d1 * 10 + d2;
-            total += line_total;
-        }
-    }
-    total
+    calibration_values
+        .iter()
+        .filter_map(|value| {
+            // An iterator, not collecting.
+            let mut d = value.chars().filter_map(|c| c.to_digit(10));
+            if let Some(first) = d.next() {
+                // chars() is a double-ended iterator, so we can use next_back.
+                // The unwrap fallback handles the case when there is only 1 digit in the value.
+                let last = d.next_back().unwrap_or(first);
+                Some(first * 10 + last)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn calibration_values_sum_v2(calibration_values: &[&str]) -> u32 {
-    let strings = [
+    const STRING_TO_DIGIT: [(&str, u32); 9] = [
         ("one", 1),
         ("two", 2),
         ("three", 3),
@@ -37,7 +41,7 @@ fn calibration_values_sum_v2(calibration_values: &[&str]) -> u32 {
         // We need to make sure we replace the first number we find
         // The right calibration values for string "eighthree" is 83 and for "sevenine" is 79.
         while i < value.len() {
-            for pair in strings {
+            for pair in STRING_TO_DIGIT {
                 if s[i..].starts_with(pair.0) {
                     s.replace_range(i..=i, &pair.1.to_string());
                 }
