f0355428ca2259e9ef88bab6a8cacfbce95fe3b1
Vincent Oberle <vincent@oberle.org>
2024-06-21 19:10:53 +0300

    2023/Day 02: Refactorings

--- 2023/day02/README.md
+++ 2023/day02/README.md
@@ -3,3 +3,7 @@
 Simple as well, most work was about parsing the input.
 
 As the input is sanitized, the parsing can be made naively.
+
+## Update
+
+Updated the code to align with the format of the previous years.
--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -1,26 +1,22 @@
-// https://adventofcode.com/2023/day/2
+use std::io::{self, Read};
 
-use std::io;
-
-fn main() {
-    let stdin = io::stdin();
+fn analyze_games(games: &str) -> (u32, u32) {
     let mut sum_ids = 0;
     let mut power_sum = 0;
-    for line in stdin.lines() {
+    for line in games.lines() {
         // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        let s = line.unwrap();
-        let split: Vec<&str> = s.split(":").collect();
+        let split: Vec<&str> = line.split(':').collect();
         let game_id: u32 = split[0].strip_prefix("Game ").unwrap().parse().unwrap();
-        let reveals: Vec<&str> = split[1].split(";").collect();
+        let reveals: Vec<&str> = split[1].split(';').collect();
         let mut test = true;
 
         let mut red_count = 0;
         let mut green_count = 0;
         let mut blue_count = 0;
         for r in reveals {
-            let col: Vec<&str> = r.split(",").collect();
+            let col: Vec<&str> = r.split(',').collect();
             for c in col {
-                let x: Vec<&str> = c.trim().split(" ").collect();
+                let x: Vec<&str> = c.trim().split_ascii_whitespace().collect();
                 let cube_count: u32 = x[0].parse().unwrap();
                 let cube_color = x[1];
                 match cube_color {
@@ -51,6 +47,34 @@ fn main() {
         }
         power_sum += red_count * green_count * blue_count;
     }
-    println!("{}", sum_ids);
-    println!("{}", power_sum);
+    (sum_ids, power_sum)
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    let (sum_ids, power_sum) = analyze_games(&input);
+
+    println!("Part 1: {}", sum_ids);
+    println!("Part 2: {}", power_sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_TEST: &str = include_str!("../resources/input_test");
+
+    #[test]
+    fn test_part1() {
+        let (sum_ids, _) = analyze_games(INPUT_TEST);
+        assert_eq!(sum_ids, 8);
+    }
+
+    #[test]
+    fn test_part2() {
+        let (_, power_sum) = analyze_games(INPUT_TEST);
+        assert_eq!(power_sum, 2286);
+    }
 }
