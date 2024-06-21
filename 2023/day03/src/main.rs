4e12fecb5fdde2baf03fb73400a97f55caa594af
Vincent Oberle <vincent@oberle.org>
2024-06-21 20:07:57 +0300

    2023/Day 03: Add Schematic struct

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -1,7 +1,6 @@
-use std::{
-    collections::{HashMap, HashSet},
-    io::{self, Read},
-};
+use std::io::{self, Read};
+
+use fxhash::{FxHashMap, FxHashSet};
 
 trait Is {
     fn is(c: char) -> bool;
@@ -23,96 +22,103 @@ impl Is for IsGear {
     }
 }
 
-fn is_adj_line<I: Is>(s: &[Vec<char>], i: usize, j: usize) -> bool {
-    I::is(s[i][j])
-        || I::is(s[i][j.saturating_sub(1)])
-        || I::is(s[i][usize::min(j + 1, s[i].len() - 1)])
-}
+struct Schematic(Vec<Vec<char>>);
 
-fn is_adjacent<I: Is>(s: &[Vec<char>], i: usize, j: usize) -> bool {
-    is_adj_line::<I>(s, i, j)
-        || is_adj_line::<I>(s, i.saturating_sub(1), j)
-        || is_adj_line::<I>(s, usize::min(i + 1, s.len() - 1), j)
-}
-
-fn add_if_gear(s: &[Vec<char>], i: usize, j: usize, adj_gears: &mut Vec<(usize, usize)>) {
-    if s[i][j] == '*' {
-        adj_gears.push((i, j));
+impl From<&str> for Schematic {
+    fn from(value: &str) -> Self {
+        Self(value.lines().map(|line| line.chars().collect()).collect())
     }
 }
 
-// Finds all the '*' adjacent to this position
-fn find_adjacent_gears(s: &[Vec<char>], i: usize, j: usize) -> Vec<(usize, usize)> {
-    let mut adj_gears = Vec::new();
-    add_if_gear(s, i, j.saturating_sub(1), &mut adj_gears);
-    add_if_gear(s, i, usize::min(j + 1, s[i].len() - 1), &mut adj_gears);
-    for k in [i.saturating_sub(1), usize::min(i + 1, s.len() - 1)] {
-        add_if_gear(s, k, j.saturating_sub(1), &mut adj_gears);
-        add_if_gear(s, k, j, &mut adj_gears);
-        add_if_gear(s, k, usize::min(j + 1, s[i].len() - 1), &mut adj_gears);
+impl Schematic {
+    fn is_adj_line<I: Is>(&self, i: usize, j: usize) -> bool {
+        I::is(self.0[i][j])
+            || I::is(self.0[i][j.saturating_sub(1)])
+            || I::is(self.0[i][usize::min(j + 1, self.0[i].len() - 1)])
     }
-    adj_gears
-}
 
-fn build(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|line| line.chars().collect()).collect()
-}
+    fn is_adjacent<I: Is>(&self, i: usize, j: usize) -> bool {
+        self.is_adj_line::<I>(i, j)
+            || self.is_adj_line::<I>(i.saturating_sub(1), j)
+            || self.is_adj_line::<I>(usize::min(i + 1, self.0.len() - 1), j)
+    }
 
-fn analyze_schematic(schematic: &[Vec<char>]) -> (u32, u32) {
-    let mut total = 0;
-    // Maps the position of the '*' to the numbers around it.
-    let mut gears: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
-    schematic.iter().enumerate().for_each(|(i, line)| {
-        let mut n = 0;
-        let mut include = false;
-        let mut adj_gears: HashSet<(usize, usize)> = HashSet::new();
-        line.iter().enumerate().for_each(|(j, c)| {
-            if let Some(d) = c.to_digit(10) {
-                n = n * 10 + d;
-                if is_adjacent::<IsSymbol>(schematic, i, j) {
-                    include = true;
-                }
-                adj_gears.extend(find_adjacent_gears(schematic, i, j));
-            } else {
-                if include {
-                    total += n;
-                    for k in &adj_gears {
-                        let list = gears.entry((k.0, k.1)).or_default();
-                        list.push(n);
-                        // println!("{} added for gear {}:{}", n, k.0, k.1);
+    fn add_if_gear(&self, i: usize, j: usize, adj_gears: &mut Vec<(usize, usize)>) {
+        if self.0[i][j] == '*' {
+            adj_gears.push((i, j));
+        }
+    }
+
+    // Finds all the '*' adjacent to this position
+    fn find_adjacent_gears(&self, i: usize, j: usize) -> Vec<(usize, usize)> {
+        let mut adj_gears = Vec::new();
+        self.add_if_gear(i, j.saturating_sub(1), &mut adj_gears);
+        self.add_if_gear(i, usize::min(j + 1, self.0[i].len() - 1), &mut adj_gears);
+        for k in [i.saturating_sub(1), usize::min(i + 1, self.0.len() - 1)] {
+            self.add_if_gear(k, j.saturating_sub(1), &mut adj_gears);
+            self.add_if_gear(k, j, &mut adj_gears);
+            self.add_if_gear(k, usize::min(j + 1, self.0[i].len() - 1), &mut adj_gears);
+        }
+        adj_gears
+    }
+
+    fn analyze(&self) -> (u32, u32) {
+        let mut total = 0;
+        // Maps the position of the '*' to the numbers around it.
+        let mut gears: FxHashMap<(usize, usize), Vec<u32>> = FxHashMap::default();
+
+        for (i, line) in self.0.iter().enumerate() {
+            let mut n = 0;
+            let mut include = false;
+            let mut adj_gears: FxHashSet<(usize, usize)> = FxHashSet::default();
+            line.iter().enumerate().for_each(|(j, c)| {
+                if let Some(d) = c.to_digit(10) {
+                    n = n * 10 + d;
+                    if self.is_adjacent::<IsSymbol>(i, j) {
+                        include = true;
                     }
+                    adj_gears.extend(self.find_adjacent_gears(i, j));
+                } else {
+                    if include {
+                        total += n;
+                        for k in &adj_gears {
+                            let list = gears.entry((k.0, k.1)).or_default();
+                            list.push(n);
+                            // println!("{} added for gear {}:{}", n, k.0, k.1);
+                        }
+                    }
+                    n = 0;
+                    include = false;
+                    adj_gears.clear();
                 }
-                n = 0;
-                include = false;
-                adj_gears.clear();
-            }
-        });
-        // This is to handle the case of a number being last on the line
-        if include {
-            total += n;
-            for k in &adj_gears {
-                let list = gears.entry((k.0, k.1)).or_default();
-                list.push(n);
-                // println!("{} added for gear {}:{}", n, k.0, k.1);
+            });
+            // This is to handle the case of a number being last on the line
+            if include {
+                total += n;
+                for k in &adj_gears {
+                    let list = gears.entry((k.0, k.1)).or_default();
+                    list.push(n);
+                    // println!("{} added for gear {}:{}", n, k.0, k.1);
+                }
             }
         }
-    });
 
-    let gear_ratio_sum: u32 = gears
-        .values()
-        .filter(|v| v.len() == 2)
-        .map(|v| v[0] * v[1])
-        .sum();
+        let gear_ratio_sum: u32 = gears
+            .values()
+            .filter(|v| v.len() == 2)
+            .map(|v| v[0] * v[1])
+            .sum();
 
-    (total, gear_ratio_sum)
+        (total, gear_ratio_sum)
+    }
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let schematic = build(&input);
+    let schematic: Schematic = input.as_str().into();
 
-    let (total, gear_ratio_sum) = analyze_schematic(&schematic);
+    let (total, gear_ratio_sum) = schematic.analyze();
 
     println!("Part 1: {}", total);
     println!("Part 2: {}", gear_ratio_sum);
@@ -126,13 +132,13 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let (total, _) = analyze_schematic(&build(INPUT_TEST));
+        let (total, _) = Into::<Schematic>::into(INPUT_TEST).analyze();
         assert_eq!(total, 4361);
     }
 
     #[test]
     fn test_part2() {
-        let (_, gear_ratio_sum) = analyze_schematic(&build(INPUT_TEST));
+        let (_, gear_ratio_sum) = Into::<Schematic>::into(INPUT_TEST).analyze();
         assert_eq!(gear_ratio_sum, 467835);
     }
 }
