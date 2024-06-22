bb6f9e94ef499893e5a222329459259334bbb908
Vincent Oberle <vincent@oberle.org>
2024-06-23 00:14:10 +0300

    2023/Day 07: Merge both parts in same project

--- 2023/day07/README.md
+++ 2023/day07/README.md
@@ -5,3 +5,5 @@ A cool exercise solved with structures for the cards and hands and custom orderi
 ## Update
 
 I updated the code to match the format of the other years.
+
+I also merged both parts into the same project. There is still a lot of duplicated code however.
--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -1,46 +1,7 @@
 use std::io::{self, Read};
-use std::{cmp::Ordering, collections::HashMap};
-use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
 
-// variants are ordered by their top-to-bottom discriminant order
-#[derive(PartialOrd, Ord, PartialEq, Eq, Copy, Clone, Hash, Debug, EnumIter)]
-enum Card {
-    J, // joker, weakest one
-    C2,
-    C3,
-    C4,
-    C5,
-    C6,
-    C7,
-    C8,
-    C9,
-    T,
-    Q,
-    K,
-    A,
-}
-
-impl Card {
-    fn new(c: char) -> Self {
-        match c {
-            'A' => Self::A,
-            'K' => Self::K,
-            'Q' => Self::Q,
-            'J' => Self::J,
-            'T' => Self::T,
-            '9' => Self::C9,
-            '8' => Self::C8,
-            '7' => Self::C7,
-            '6' => Self::C6,
-            '5' => Self::C5,
-            '4' => Self::C4,
-            '3' => Self::C3,
-            '2' => Self::C2,
-            _ => panic!("Invalid char"),
-        }
-    }
-}
+mod base;
+mod joker;
 
 #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
 enum HandType {
@@ -70,154 +31,30 @@ impl HandType {
     }
 }
 
-#[derive(PartialEq, Eq, Debug)]
-struct Hand {
-    cards: Vec<Card>, // len is always 5
-    hand_type: HandType,
-}
-
-impl Hand {
-    fn new(s: &str) -> Self {
-        let cards: Vec<Card> = s.chars().map(Card::new).collect();
-        let hand_type = Self::recognize(&cards);
-        Self { cards, hand_type }
-    }
-
-    fn recognize(cards: &[Card]) -> HandType {
-        let mut highest_hand_type = HandType::HighCard;
-        if Self::contains_joker(cards) {
-            for replacement_card in Card::iter() {
-                let mut cards_copy = cards.to_vec();
-                for c in &mut cards_copy.iter_mut() {
-                    if *c == Card::J {
-                        *c = replacement_card;
-                    }
-                }
-                highest_hand_type = highest_hand_type.max(Self::find_hand_type(&cards_copy));
-            }
-        }
-        highest_hand_type.max(Self::find_hand_type(cards))
-    }
-
-    fn find_hand_type(cards: &[Card]) -> HandType {
-        let frequencies_map = cards.iter().fold(HashMap::new(), |mut map, val| {
-            map.entry(val).and_modify(|frq| *frq += 1).or_insert(1);
-            map
-        });
-        let mut frequencies: Vec<i32> = frequencies_map.values().copied().collect();
-        frequencies.sort_unstable();
-        frequencies.reverse();
-        HandType::new(&frequencies)
-    }
-
-    fn contains_joker(cards: &[Card]) -> bool {
-        cards.contains(&Card::J)
-    }
-}
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let ord = self.hand_type.cmp(&other.hand_type);
-        if ord == Ordering::Equal {
-            self.cards.iter().cmp(other.cards.iter())
-        } else {
-            ord
-        }
-    }
-}
-
-#[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
-struct HandBid {
-    hand: Hand,
-    bid: u32,
-}
-
-impl HandBid {
-    fn new(hand: &str, bid: u32) -> Self {
-        Self {
-            hand: Hand::new(hand),
-            bid,
-        }
-    }
-}
-
-fn build(input: &str) -> Vec<HandBid> {
-    let mut hands: Vec<HandBid> = input
-        .lines()
-        .map(|line| {
-            let v: Vec<&str> = line.split_whitespace().collect();
-            HandBid::new(v[0], v[1].parse().unwrap())
-        })
-        .collect();
-
-    hands.sort();
-    hands
-}
-
-fn new_total_winnings(hands: &[HandBid]) -> u32 {
-    hands
-        .iter()
-        .enumerate()
-        .map(|(i, hand)| (u32::try_from(i).unwrap() + 1) * hand.bid)
-        .sum()
-}
-
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let hands = build(&input);
 
-    // println!("Part 1: {}", part1(&input_parsed));
-    println!("Part 2: {}", new_total_winnings(&hands));
+    let hands = base::build(&input);
+    println!("Part 1: {}", base::total_winnings(&hands));
+
+    let hands = joker::build(&input);
+    println!("Part 2: {}", joker::total_winnings(&hands));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn check_recognize() {
-        assert_eq!(Hand::new("AAAAA").hand_type, HandType::FiveOfAKind);
-        assert_eq!(Hand::new("AA8AA").hand_type, HandType::FourOfAKind);
-        assert_eq!(Hand::new("23332").hand_type, HandType::FullHouse);
-        assert_eq!(Hand::new("TTT98").hand_type, HandType::ThreeOfAKind);
-        assert_eq!(Hand::new("23432").hand_type, HandType::TwoPair);
-        assert_eq!(Hand::new("A23A4").hand_type, HandType::OnePair);
-        assert_eq!(Hand::new("23456").hand_type, HandType::HighCard);
-    }
-
-    #[test]
-    fn check_ordering() {
-        let mut hands = vec![
-            Hand::new("32T3K"),
-            Hand::new("T55J5"),
-            Hand::new("KK677"),
-            Hand::new("KTJJT"),
-            Hand::new("QQQJA"),
-        ];
-        hands.sort();
-        assert_eq!(hands[0], Hand::new("32T3K"));
-        assert_eq!(hands[1], Hand::new("KK677"));
-        assert_eq!(hands[2], Hand::new("T55J5"));
-        assert_eq!(hands[3], Hand::new("QQQJA"));
-        assert_eq!(hands[4], Hand::new("KTJJT"));
-    }
-
     const INPUT_TEST: &str = include_str!("../resources/input_test");
 
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(part1(&build(INPUT_TEST)), 0);
-    // }
+    #[test]
+    fn test_part1() {
+        assert_eq!(base::total_winnings(&base::build(INPUT_TEST)), 6440);
+    }
 
     #[test]
     fn test_part2() {
-        assert_eq!(new_total_winnings(&build(INPUT_TEST)), 5905);
+        assert_eq!(joker::total_winnings(&joker::build(INPUT_TEST)), 5905);
     }
 }
