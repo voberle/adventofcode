c44a5e0238cf7906f8a45c9f145abd1f22a21af9
Vincent Oberle <vincent@oberle.org>
2024-06-22 21:26:11 +0300

    2023/Day 05: Part 2

--- 2023/day05/README.md
+++ 2023/day05/README.md
@@ -5,3 +5,11 @@ Things got suddenly much harder on day 5. Part 1 took a bit of work but wasn't t
 Unfortunately I didn't find the trick to do it efficiently and somehow brute forced it by running it manually on various ranges until I found the value.
 
 First use of RegEx as well.
+
+## Update
+
+After doing the older years, I came back to this problem.
+
+I update the code and fixed the part 2. This time, I quickly got the idea of doing a binary search with a recursive function. The main trick is that we can use the following optimization: If the difference between the location ranges and seed ranges is the same, then the location beginning is also the smallest.
+
+With that, both parts run in 3.5 ms.
--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -47,47 +47,6 @@ fn convert(maps: &SeedMap, seed: u64) -> u64 {
     n
 }
 
-#[derive(Debug)]
-struct SeedRanges {
-    ranges: Vec<Range<u64>>,
-}
-
-#[allow(dead_code)]
-impl SeedRanges {
-    fn new(ranges: Vec<Range<u64>>) -> Self {
-        let mut s = Self { ranges };
-        s.ranges.sort_by_key(|r| r.start);
-        s
-    }
-
-    fn first(&self) -> u64 {
-        self.ranges[0].start
-    }
-
-    fn add(&self, val: u64, to_add: u64) -> Result<u64, &'static str> {
-        let mut res = val;
-        let mut inc = to_add;
-        let mut idx: usize = self
-            .ranges
-            .iter()
-            .enumerate()
-            .find(|(_, r)| r.contains(&val))
-            .unwrap()
-            .0;
-        // println!("{val}");
-        while !self.ranges[idx].contains(&(res + inc)) {
-            inc -= self.ranges[idx].end - res;
-            idx += 1;
-            if idx >= self.ranges.len() {
-                return Err("Reached end of ranges");
-            }
-            res = self.ranges[idx].start;
-        }
-        res += inc;
-        Ok(res)
-    }
-}
-
 // Parses the input, returning the list of seeds and the seed maps.
 fn build(input: &str) -> (Vec<u64>, SeedMap) {
     let mut it = input.lines();
@@ -125,9 +84,6 @@ fn build(input: &str) -> (Vec<u64>, SeedMap) {
             ));
         }
     }
-    // println!("Seeds {:?}", seeds);
-    // println!("Maps {:#?}", maps);
-
     (seeds, maps)
 }
 
@@ -135,38 +91,41 @@ fn lowest_location_v1(seeds: &[u64], maps: &SeedMap) -> u64 {
     seeds.iter().map(|seed| convert(maps, *seed)).min().unwrap()
 }
 
-fn lowest_location_v2(seeds: &[u64], maps: &SeedMap) -> u64 {
-    // Very crude brute force way of doing it.
-    // First with a big STEP, identify which range most likely has the lowest.
-    // Then on this range only decrease the step until 1 to get the lowest.
-    const STEP: u64 = 100_000;
-    // const STEP: u64 = 1;
-
+// Get the seed ranges used by part 2.
+fn get_initial_seed_ranges(seeds: &[u64]) -> Vec<Range<u64>> {
     let mut seed_ranges: Vec<Range<u64>> = seeds.chunks(2).map(|c| (c[0]..c[0] + c[1])).collect();
     seed_ranges.sort_by_key(|r| r.start);
-    println!("Seed ranges {:#?}", seed_ranges);
+    seed_ranges
+}
 
-    let mut location = u64::MAX;
-    let mut lowest_seed_idx = 0;
+// Recursive.
+// Idea is to binary-search it basically, and whenever we have a range where the
+// difference in source [beginning,end] is same as destination [beginning,end],
+// then the minimum is beginning.
+fn lowest_location_between(maps: &SeedMap, low_seed: u64, high_seed: u64) -> u64 {
+    if low_seed == high_seed {
+        return convert(maps, low_seed);
+    }
+
+    let low_loc = convert(maps, low_seed);
+    let high_loc = convert(maps, high_seed);
+    if low_seed <= high_seed && low_loc <= high_loc && (high_loc - low_loc == high_seed - low_seed)
+    {
+        return low_loc;
+    }
+    let mid_seed = ((high_seed - low_seed) / 2) + low_seed;
+    let low_part = lowest_location_between(maps, low_seed, mid_seed);
+    let high_part = lowest_location_between(maps, mid_seed + 1, high_seed);
+    low_part.min(high_part)
+}
+
+fn lowest_location_v2(seeds: &[u64], maps: &SeedMap) -> u64 {
+    let seed_ranges = get_initial_seed_ranges(seeds);
     seed_ranges
         .iter()
-        .enumerate()
-        // .filter(|(i, _)| *i == 9)
-        .for_each(|(i, range)| {
-            let mut seed = range.start;
-            while seed < range.end {
-                let r = convert(maps, seed);
-                if r < location {
-                    lowest_seed_idx = i;
-                    location = r;
-                    println!("In {:?} found {}", range, location);
-                }
-                seed += STEP;
-            }
-        });
-    println!("Idx: {}: {}", lowest_seed_idx, location);
-
-    location
+        .map(|range| lowest_location_between(maps, range.start, range.end - 1))
+        .min()
+        .unwrap()
 }
 
 fn main() {
@@ -176,7 +135,7 @@ fn main() {
     let (seeds, maps) = build(&input);
 
     println!("Part 1: {}", lowest_location_v1(&seeds, &maps));
-    // println!("Part 2: {}", lowest_location_v2(&seeds, &maps));
+    println!("Part 2: {}", lowest_location_v2(&seeds, &maps));
 }
 
 #[cfg(test)]
@@ -196,17 +155,6 @@ mod tests {
         let r2 = RangeMapping::new(52, 50, 48);
         assert!(r2.is_in(53));
         assert_eq!(r2.convert(53), 55);
-    }
-
-    #[test]
-    fn check_seed_range() {
-        let ranges = vec![79..(79 + 14), 55..(55 + 13)];
-        let s = SeedRanges::new(ranges);
-        assert_eq!(s.first(), 55);
-        assert_eq!(s.add(55, 2), Ok(57));
-        assert_eq!(s.add(57, 13), Ok(81));
-        assert_eq!(s.add(81, 11), Ok(92));
-        assert!(s.add(92, 1).is_err());
     }
 
     #[test]
