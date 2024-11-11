a1a6a8990eec05c71e6c2ea2155d6e46212ae109
Vincent Oberle <vincent@oberle.org>
2024-11-11 17:30:13 +0200

    2023/Day 09: Optimizations

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -7,30 +7,27 @@ fn build(input: &str) -> Vec<Vec<i32>> {
         .collect()
 }
 
-fn part1_2(report: Vec<Vec<i32>>) -> (i32, i32) {
-    let mut sum_extrapolated_values_begin = 0;
-    let mut sum_extrapolated_values_end = 0;
+fn sum_extrapolated_values(report: Vec<Vec<i32>>) -> (i32, i32) {
+    let mut sum_begin = 0;
+    let mut sum_end = 0;
+
+    // Going through each line in the report.
     for history in report {
         let mut first_item: Vec<i32> = Vec::new();
-        let mut last_item: Vec<i32> = Vec::new();
         let mut next_line: Vec<i32> = history.clone();
-        // println!("{:?}", next_line);
+
+        // Generate new lines until it's full of zeroes.
         while next_line.iter().any(|i| *i != 0) {
             first_item.push(*next_line.first().unwrap());
-            last_item.push(*next_line.last().unwrap());
+            sum_end += next_line.last().unwrap();
             next_line = next_line.windows(2).map(|w| w[1] - w[0]).collect();
-            // println!("{:?}", next_line);
         }
 
-        sum_extrapolated_values_end += last_item.iter().sum::<i32>();
-
         first_item.reverse();
-        // println!("first_item {:?}", first_item);
         let first_val = first_item.iter().fold(0, |n, i| i - n);
-        // println!("first_val {}", first_val);
-        sum_extrapolated_values_begin += first_val;
+        sum_begin += first_val;
     }
-    (sum_extrapolated_values_end, sum_extrapolated_values_begin)
+    (sum_end, sum_begin)
 }
 
 fn main() {
@@ -38,10 +35,10 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let report = build(&input);
 
-    let (sum_extrapolated_values_end, sum_extrapolated_values_begin) = part1_2(report);
+    let (sum_end, sum_begin) = sum_extrapolated_values(report);
 
-    println!("Part 1: {}", sum_extrapolated_values_end);
-    println!("Part 2: {}", sum_extrapolated_values_begin);
+    println!("Part 1: {}", sum_end);
+    println!("Part 2: {}", sum_begin);
 }
 
 #[cfg(test)]
@@ -51,9 +48,10 @@ mod tests {
     const INPUT_TEST: &str = include_str!("../resources/input_test");
 
     #[test]
-    fn test_part1_2() {
+    fn test_sum_extrapolated_values() {
         let report = build(INPUT_TEST);
-        let (sum_extrapolated_values_end, sum_extrapolated_values_begin) = part1_2(report);
+        let (sum_extrapolated_values_end, sum_extrapolated_values_begin) =
+            sum_extrapolated_values(report);
 
         assert_eq!(sum_extrapolated_values_end, 114);
         assert_eq!(sum_extrapolated_values_begin, 2);
