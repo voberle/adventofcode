4e1a8c381b90b7778b04dc20fd8d2440a32c4e31
Vincent Oberle <vincent@oberle.org>
2024-12-02 10:34:12 +0200

    2024/Day 02: Few simplifications

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -12,24 +12,23 @@ impl Report {
     }
 
     fn is_safe(&self) -> bool {
-        let mut prev = self.0[0];
-        let mut sign: i64 = 0;
+        let mut prev: u32 = *self.0.first().unwrap();
+        let mut direction: i64 = 0; // 1 if increasing, -1 if decreasing
+
         for n in self.0.iter().skip(1) {
             let diff: i64 = i64::from(*n) - i64::from(prev);
-            if sign != 0 {
-                if diff * sign < 0 {
-                    // Direction is changing
+            if direction != 0 {
+                if diff * direction < 0 {
+                    // Difference and direction have different sign, it means direction is changing and report is unsafe.
                     return false;
                 }
-            } else if diff < 0 {
-                sign = -1;
-            } else if diff > 0 {
-                sign = 1;
             } else {
-                return false;
+                // Direction isn't set, setting it.
+                direction = diff.signum();
             }
 
             if !(1..=3).contains(&diff.abs()) {
+                // Levels differ by too much, report is unsafe.
                 return false;
             }
 
@@ -89,6 +88,12 @@ mod tests {
     use super::*;
 
     const INPUT_TEST: &str = include_str!("../resources/input_test_1");
+
+    #[test]
+    fn check_one_element_report() {
+        // Input doesn't have one element reports, just making sure code doesn't crash on it.
+        assert!(Report::new("2").is_safe());
+    }
 
     #[test]
     fn test_part1() {
