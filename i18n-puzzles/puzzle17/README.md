1382c82ebf04d82504b4a81a96a0ca6f7583804b
Vincent Oberle <vincent@oberle.org>
2025-04-05 20:51:05 +0300

    i18: Puzzle 17

--- i18n-puzzles/puzzle17/README.md
+++ i18n-puzzles/puzzle17/README.md
@@ -1,2 +1,16 @@
 # Puzzle 17: [╳ marks the spot](https://i18n-puzzles.com/puzzle/17/)
 
+This one turned out way more tricky to solve than it appeared initially.
+
+There were two main challenges.
+
+- First the map fragments have the same width, but different heights, which makes it tricky to build a partial map.
+- Second, while it was relatively easy to check if two fragments fit horizontally, it wasn't possible to say if they fit vertically.
+
+So to solve it, I used the fact that it was possible to identify which fragments were on the left border by looking for the border characters. This didn't give me their order, but fortunately they were not that many permutations: Removing the top and bottom corners, there was only 3 left border fragments and I could try all possible permutations of these left border fragments.
+
+For each permutation, I build the left "column", and then I attempt to build the next column, from top to down. For this, I try to add fragments, converting the resulting map to to Unicode and seeing if it's valid one.
+
+Checking if such a partial map is valid Unicode is done with the `String::from_utf8_lossy` function, which converts a Vec of u8 to a String, using the replacement character for invalid one. I ignore all the replacement characters at the end of the lines (since it's only a partial map) and check if there are any in the middle of the map. If not, the partial map is valid so far.
+
+The loop that builds the columns is a bit too complex I feel.
--- i18n-puzzles/puzzle17/src/main.rs
+++ i18n-puzzles/puzzle17/src/main.rs
@@ -1,8 +1,14 @@
-use std::io::{self, Read};
+use std::{
+    char::REPLACEMENT_CHARACTER,
+    io::{self, Read},
+};
 
 use itertools::Itertools;
 
-fn line_to_u8(line: &str) -> Vec<u8> {
+type Fragment = Vec<Vec<u8>>;
+
+// Converts a string with hexadecimals to a vector of bytes.
+fn hex_line_to_u8(line: &str) -> Vec<u8> {
     (0..line.len())
         .step_by(2)
         .map(|i| {
@@ -12,43 +18,202 @@ fn line_to_u8(line: &str) -> Vec<u8> {
         .collect()
 }
 
-fn bloc_to_u8(bloc: &str) -> Vec<Vec<u8>> {
-    bloc.lines().map(line_to_u8).collect()
+// Converts a string of hexadecimals lines to a fragment (a vec of vec of bytes).
+fn hexa_bloc_to_fragment(bloc: &str) -> Fragment {
+    bloc.lines().map(hex_line_to_u8).collect()
 }
 
 // Parses the input as a list of fragments, each fragment being a list of bytes.
-fn build(input: &str) -> Vec<Vec<Vec<u8>>> {
+fn build(input: &str) -> Vec<Fragment> {
     let fragments: Vec<_> = input.split("\n\n").collect();
     fragments
         .iter()
-        .map(|fragment| bloc_to_u8(fragment))
+        .map(|fragment| hexa_bloc_to_fragment(fragment))
         .collect()
 }
 
-fn fragment_to_string(fragment: &[Vec<u8>]) -> String {
+// Converts the bloc of u8 into a String bloc of text (with new lines).
+// Invalid UTF-8 blocs are replaced with replacement character.
+fn fragment_to_string_lossy(fragment: &Fragment) -> String {
     fragment
         .iter()
-        .map(|line| {
-            // println!("{:02x?}", line);
-            String::from_utf8_lossy(line)
-        })
+        .map(|line| String::from_utf8_lossy(line))
         .join("\n")
 }
 
-fn x_product(map_sample: &[Vec<Vec<u8>>]) -> usize {
-    for fragment in map_sample {
-        println!("{}", fragment_to_string(fragment));
-        println!();
+// Checks if the fragment (a partial map) is valid UTF-8.
+// This is done by ignoring any possible replacement chars at the end of each line.
+fn is_valid_utf8_inside(fragment: &Fragment) -> bool {
+    fragment.iter().all(|line| {
+        let mut lossy = String::from_utf8_lossy(line.trim_ascii_end()).into_owned();
+        if let Some(last) = lossy.chars().last() {
+            if last == REPLACEMENT_CHARACTER {
+                lossy.pop();
+            }
+        }
+        !lossy.contains(REPLACEMENT_CHARACTER)
+    })
+}
+
+// Finds the left corners and returns their indexes.
+fn find_left_corners(map_samples: &[Fragment]) -> (usize, usize) {
+    let (mut top_left, mut bottom_left) = (None, None);
+    // Go through each fragment and check if they contain the corner characters.
+    for (index, fragment) in map_samples.iter().enumerate() {
+        let first_line = String::from_utf8_lossy(fragment.first().unwrap());
+        if let Some(first) = first_line.chars().next() {
+            if first == '╔' {
+                assert!(top_left.is_none());
+                top_left = Some(index);
+            }
+        }
+
+        let last_line = String::from_utf8_lossy(fragment.last().unwrap());
+        if let Some(first) = last_line.chars().next() {
+            if first == '╚' {
+                assert!(bottom_left.is_none());
+                bottom_left = Some(index);
+            }
+        }
     }
-    0
+
+    (top_left.unwrap(), bottom_left.unwrap())
+}
+
+// Check if this string fragment is part of the left border.
+fn is_left_border_str(s: &str) -> bool {
+    s.lines().any(|line| {
+        let f = line.chars().next().unwrap();
+        ['╔', '|', '║', '╚'].contains(&f)
+    })
+}
+
+// Find all the fragments that are on the left border.
+fn find_left_border(map_samples: &[Fragment]) -> Vec<usize> {
+    map_samples
+        .iter()
+        .enumerate()
+        .filter_map(|(index, fragment)| {
+            let s = fragment_to_string_lossy(fragment);
+            if is_left_border_str(&s) {
+                Some(index)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+// Add the specified fragment to the bigger map in construction.
+// The fragment is added at the specified y position, after the most right fragment.
+fn add_fragment_at(big_map: &mut Fragment, fragment: &Fragment, y_pos: usize) {
+    for y in 0..fragment.len() {
+        big_map[y_pos + y].extend(fragment[y].clone());
+    }
+}
+
+// Find the ╳.
+fn find_x_position_product(map: &Fragment) -> usize {
+    let map_as_str = fragment_to_string_lossy(map);
+    for (y, line) in map_as_str.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '╳' {
+                return x * y;
+            }
+        }
+    }
+    panic!("X not found")
+}
+
+// Finds the position of the X.
+fn x_product(map_samples: &[Fragment]) -> usize {
+    // The samples have all the same width but different heights.
+
+    // We can find all the fragments that go on the left.
+    // We know the top one and bottom one. There are not that many in the middle, so
+    // not that many permutations.
+    // So we pick a permutation, and try to fill the next column.
+
+    let (top_left, bottom_left) = find_left_corners(map_samples);
+
+    let left_border = find_left_border(map_samples);
+
+    let left_border_without_corner: Vec<usize> = left_border
+        .iter()
+        .filter(|i| ![top_left, bottom_left].contains(i))
+        .copied()
+        .collect();
+
+    let mut big_map = Vec::new();
+
+    for perm in left_border_without_corner
+        .iter()
+        .permutations(left_border_without_corner.len())
+    {
+        // For this permutation, build the left column.
+        big_map.clone_from(&map_samples[top_left]);
+        for i in perm {
+            big_map.extend(map_samples[*i].clone());
+        }
+        big_map.extend(map_samples[bottom_left].clone());
+
+        assert!(is_valid_utf8_inside(&big_map));
+
+        // Indexes of the fragments we still need to locate.
+        let mut remaining: Vec<usize> = (0..map_samples.len()).collect();
+        remaining.retain(|i| !left_border.contains(i));
+
+        while !remaining.is_empty() {
+            // Try to build the next column
+            let mut current_y = 0;
+
+            'outer_col: while current_y < big_map.len() {
+                let mut added_to_this_col = Vec::new();
+                for fragment_index in &remaining {
+                    // The fragment may be too high to fit.
+                    if current_y + map_samples[*fragment_index].len() > big_map.len() {
+                        continue;
+                    }
+
+                    // Create a copy of the map, add the fragment and check if it's valid.
+                    let mut map_copy = big_map.clone();
+
+                    add_fragment_at(&mut map_copy, &map_samples[*fragment_index], current_y);
+
+                    if is_valid_utf8_inside(&map_copy) {
+                        // If it fits, we take the copy of the map as new reference, and look for next one.
+                        big_map = map_copy;
+                        added_to_this_col.push(*fragment_index);
+                        current_y += map_samples[*fragment_index].len();
+                    }
+                }
+
+                if added_to_this_col.is_empty() {
+                    // None found, we have wrong permutation
+                    break 'outer_col;
+                }
+                remaining.retain(|i| !added_to_this_col.contains(i));
+            }
+
+            if current_y == big_map.len() {
+                // Filled the column
+                continue;
+            }
+
+            // We couldn't fill the column with this permutation, trying next one.
+            break;
+        }
+    }
+
+    find_x_position_product(&big_map)
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let map_sample = build(&input);
+    let map_samples = build(&input);
 
-    println!("Answer: {}", x_product(&map_sample));
+    println!("Answer: {}", x_product(&map_samples));
 }
 
 #[cfg(test)]
@@ -60,7 +225,7 @@ mod tests {
 
     #[test]
     fn test_hex_to_utf8() {
-        let bin = line_to_u8("e29594");
+        let bin = hex_line_to_u8("e29594");
         let string_utf8_lossy = String::from_utf8_lossy(&bin);
         println!("{}", string_utf8_lossy);
         assert_eq!(string_utf8_lossy, "╔");
@@ -68,18 +233,18 @@ mod tests {
 
     #[test]
     fn test_fragment_to_string() {
-        let fragment = bloc_to_u8(INPUT_TEST_2);
+        let fragment = hexa_bloc_to_fragment(INPUT_TEST_2);
         let expected = r"╔-═-═-═-
 |~≋≋ññ≋~
 ║ññ≋~~≋�
 |~ññ𑀍ñ≋�";
-        let s = fragment_to_string(&fragment);
+        let s = fragment_to_string_lossy(&fragment);
         assert_eq!(s, expected)
     }
 
     #[test]
     fn test_answer() {
-        let map_sample = build(INPUT_TEST_1);
-        assert_eq!(x_product(&map_sample), 132);
+        let map_samples = build(INPUT_TEST_1);
+        assert_eq!(x_product(&map_samples), 132);
     }
 }
