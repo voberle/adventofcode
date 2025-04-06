1e7b97dab8718250c09d621e5c928883f252689a
Vincent Oberle <vincent@oberle.org>
2025-04-06 22:55:34 +0300

    i18: Puzzle 18: Calculation without BiDi chars

---
+++ i18n-puzzles/puzzle18/README.md
@@ -0,0 +1,6 @@
+# Puzzle 18: [Rex To Lynx](https://i18n-puzzles.com/puzzle/18/)
+
+I reused code from previous AoC days:
+
+- The expression evaluator from [AoC year 2020 day 18](../../2020/day18/README.md). The parsing was not usable however as it supported only single digit numbers.
+- But in [AoC year 2022 day 13](../../2022/day13/README.md) we had the parsing.
---
+++ i18n-puzzles/puzzle18/src/main.rs
@@ -0,0 +1,195 @@
+use std::io::{self, Read};
+
+use deunicode::deunicode;
+
+fn build(input: &str) -> Vec<String> {
+    input.lines().map(std::string::ToString::to_string).collect()
+}
+
+fn remove_bidi_chars(s: &str) -> String {
+    deunicode(s)
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Token {
+    Number(u64),
+    OpenParenthesis,
+    CloseParenthesis,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+}
+use Token::{CloseParenthesis, Divide, Minus, Multiply, Number, OpenParenthesis, Plus};
+
+fn str_to_tokens(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut current_number = String::new();
+
+    for c in input.chars() {
+        if c.is_ascii_digit() {
+            current_number.push(c);
+        } else {
+            if !current_number.is_empty() {
+                tokens.push(Token::Number(current_number.parse().unwrap()));
+                current_number.clear();
+            }
+            if c == ' ' {
+                continue;
+            }
+            tokens.push(match c {
+                '(' => Token::OpenParenthesis,
+                ')' => Token::CloseParenthesis,
+                '+' => Token::Plus,
+                '-' => Token::Minus,
+                '*' => Token::Multiply,
+                '/' => Token::Divide,
+                _ => panic!("Invalid char"),
+            });
+        }
+    }
+    if !current_number.is_empty() {
+        tokens.push(Token::Number(current_number.parse().unwrap()));
+    }
+    tokens
+}
+
+fn precedence(token: &Token) -> u8 {
+    match token {
+        Multiply | Divide => 2,
+        Plus | Minus => 1,
+        _ => 0,
+    }
+}
+
+fn calculate(tokens: &[Token]) -> u64 {
+    // Implementation of the Dijkstra Shunting Yard Algorithm
+    // Based on the pseudo-code from https://www.geeksforgeeks.org/expression-evaluation/
+
+    // Values and operators stack.
+    let mut values: Vec<u64> = Vec::new();
+    let mut operators: Vec<Token> = Vec::new();
+
+    for token in tokens {
+        // Go through each token in order.
+        match token {
+            Number(n) => values.push(*n),
+            OpenParenthesis => operators.push(*token),
+            CloseParenthesis => {
+                // While the top of the operator stack is not a open parenthesis.
+                while !matches!(operators.last(), Some(OpenParenthesis)) {
+                    let operator = operators.pop().unwrap();
+                    let val2 = values.pop().unwrap();
+                    let val1 = values.pop().unwrap();
+                    let result = match operator {
+                        Plus => val1 + val2,
+                        Minus => val1 - val2,
+                        Multiply => val1 * val2,
+                        Divide => val1 / val2,
+                        _ => panic!("Invalid operator: {operator:?}"),
+                    };
+                    // Push the result onto the value stack.
+                    values.push(result);
+                }
+                // Pop the open parenthesis from the operator stack, and discard it.
+                operators.pop();
+            }
+            Plus | Minus | Multiply | Divide => {
+                // While the operator stack is not empty, and the top has the same or greater precedence as thisOp,
+                while !operators.is_empty()
+                    && precedence(operators.last().unwrap()) >= precedence(token)
+                {
+                    let operator = operators.pop().unwrap();
+                    let val2 = values.pop().unwrap();
+                    let val1 = values.pop().unwrap();
+                    let result = match operator {
+                        Plus => val1 + val2,
+                        Minus => val1 - val2,
+                        Multiply => val1 * val2,
+                        Divide => val1 / val2,
+                        _ => panic!("Invalid operator: {operator:?}"),
+                    };
+                    values.push(result);
+                }
+                operators.push(*token);
+            }
+        }
+    }
+
+    // While the operator stack is not empty.
+    while let Some(operator) = operators.pop() {
+        let val2 = values.pop().unwrap();
+        let val1 = values.pop().unwrap();
+        let result = match operator {
+            Plus => val1 + val2,
+            Minus => val1 - val2,
+            Multiply => val1 * val2,
+            Divide => val1 / val2,
+            _ => panic!("Invalid operator: {operator:?}"),
+        };
+        values.push(result);
+    }
+
+    // At this point the operator stack should be empty, and the value stack has one value, the final result.
+    assert_eq!(values.len(), 1);
+    values[0]
+}
+
+fn scams_sum(lines: &[String]) -> u64 {
+    0
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let lines = build(&input);
+
+    println!("Answer: {}", scams_sum(&lines));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_TEST_1: &str = include_str!("../resources/input_test_1");
+
+    #[test]
+    fn test_bidi_removal() {
+        let input = "\u{2067}(1 * ((\u{2066}(66 / 2)\u{2069} - 15) - 4)) * (1 + (1 + 1))\u{2069}";
+        assert_eq!(
+            remove_bidi_chars(input),
+            "(1 * (((66 / 2) - 15) - 4)) * (1 + (1 + 1))"
+        );
+    }
+
+    #[test]
+    fn test_calculate() {
+        let s = "(1 * (((66 / 2) - 15) - 4)) * (1 + (1 + 1))";
+        let tokens = str_to_tokens(s);
+        // println!("{:?}", tokens);
+        assert_eq!(calculate(&tokens), 42);
+    }
+
+    #[test]
+    fn test_rex_calculation() {
+        fn calc(line: &str) -> u64 {
+            calculate(&str_to_tokens(&remove_bidi_chars(line)))
+        }
+
+        // Calculate by ignoring BiDi chars.
+        let lines = build(INPUT_TEST_1);
+
+        assert_eq!(calc(&lines[0]), 42);
+        assert_eq!(calc(&lines[1]), 260);
+        assert_eq!(calc(&lines[2]), 15040);
+        assert_eq!(calc(&lines[3]), 6300);
+        assert_eq!(calc(&lines[4]), 2760);
+        assert_eq!(calc(&lines[5]), 316);
+    }
+
+    #[test]
+    fn test_answer() {
+        let lines = build(INPUT_TEST_1);
+        assert_eq!(scams_sum(&lines), 19282);
+    }
+}
