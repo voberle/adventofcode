1b21e401ca9e6fa5f4269543e31755ca5191349c
Vincent Oberle <vincent@oberle.org>
2025-04-23 20:23:11 +0300

    Codyssi: 2024/03: Part 3

--- codyssi/2024/puzzle03/src/main.rs
+++ codyssi/2024/puzzle03/src/main.rs
@@ -32,6 +32,34 @@ fn readings_base10_sum(readings: &[Reading]) -> u64 {
     readings.iter().map(Reading::value).sum()
 }
 
+fn to_base65(n: u64) -> String {
+    #[rustfmt::skip]
+    const CHARS: [char; 65] = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '!', '@', '#', 
+    ];
+
+    // We use a Vec instead of a String, as we need to reverse it at the end.
+    let mut result = Vec::new();
+
+    let mut n = n;
+    while n != 0 {
+        let rem = n % 65;
+        result.push(CHARS[rem as usize]);
+        n /= 65;
+    }
+    result.reverse();
+
+    result.iter().collect()
+}
+
+fn readings_base65_sum(readings: &[Reading]) -> String {
+    let sum = readings_base10_sum(readings);
+    to_base65(sum)
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -39,7 +67,7 @@ fn main() {
 
     println!("Part 1: {}", composition_sum(&readings));
     println!("Part 2: {}", readings_base10_sum(&readings));
-    // println!("Part 3: {}", part3(&values));
+    println!("Part 3: {}", readings_base65_sum(&readings));
 }
 
 #[cfg(test)]
@@ -59,5 +87,7 @@ mod tests {
     }
 
     #[test]
-    fn test_part3() {}
+    fn test_part3() {
+        assert_eq!(readings_base65_sum(&build(INPUT_TEST)), "30PzDC");
+    }
 }
