4bc67a5ec844a97929c3d476346cfae9c9ace36f
Vincent Oberle <vincent@oberle.org>
2025-04-23 22:18:07 +0300

    Codyssi: 2024/04: Part 3, cleanups

--- codyssi/2024/puzzle04/src/main.rs
+++ codyssi/2024/puzzle04/src/main.rs
@@ -1,39 +1,35 @@
 use std::{
     collections::{HashMap, HashSet},
     io::{self, Read},
+    iter::once,
 };
-
-fn build(input: &str) -> Vec<Vec<String>> {
-    input
-        .lines()
-        .map(|line| line.split(" <-> ").map(ToString::to_string).collect())
-        .collect()
-}
-
-fn unique_locations_count(paths: &[Vec<String>]) -> usize {
-    paths.iter().flatten().collect::<HashSet<&String>>().len()
-}
 
 // All connections for each locations.
 type Connections = HashMap<String, HashSet<String>>;
 
-fn create_connection_map(paths: &[Vec<String>]) -> Connections {
+fn create_connection_map(input: &str) -> Connections {
     let mut connections: Connections = HashMap::default();
-    for path in paths {
-        for (p1, p2) in [(&path[0], &path[1]), (&path[1], &path[0])] {
+    for line in input.lines() {
+        let path: Vec<&str> = line.split(" <-> ").collect();
+
+        for (p1, p2) in [(path[0], path[1]), (path[1], path[0])] {
             connections
-                .entry(p1.clone())
+                .entry(p1.to_string())
                 .and_modify(|conns| {
-                    conns.insert(p2.clone());
+                    conns.insert(p2.to_string());
                 })
                 .or_insert({
                     let mut set = HashSet::default();
-                    set.insert(p2.clone());
+                    set.insert(p2.to_string());
                     set
                 });
         }
     }
     connections
+}
+
+fn unique_locations_count(connections: &Connections) -> usize {
+    connections.len()
 }
 
 // Returns what can be reached directly from this set of locations.
@@ -46,37 +42,32 @@ fn reached_from(connections: &Connections, from: &HashSet<String>) -> HashSet<St
     next
 }
 
-fn unique_locations_under(paths: &[Vec<String>], from: &str, time: usize) -> usize {
-    let connections = create_connection_map(paths);
-
-    let mut reached: HashSet<String> = HashSet::default();
-    reached.insert(from.to_string());
+fn unique_locations_under(connections: &Connections, from: &str, time: usize) -> usize {
+    let mut reached: HashSet<String> = once(from.to_string()).collect();
 
     let mut t = 0;
     while t < time {
-        reached = reached_from(&connections, &reached);
+        reached = reached_from(connections, &reached);
         t += 1;
     }
 
     reached.len()
 }
 
-fn part2(paths: &[Vec<String>]) -> usize {
-    unique_locations_under(paths, "STT", 3)
+fn part2(connections: &Connections) -> usize {
+    unique_locations_under(connections, "STT", 3)
 }
 
-fn shortests_time_from(paths: &[Vec<String>], from: &str) -> usize {
-    let connections = create_connection_map(paths);
-
+fn shortests_time_from(connections: &Connections, from: &str) -> usize {
     let mut times: HashMap<String, usize> = HashMap::default();
     times.insert(from.to_string(), 0);
 
-    let mut reached: HashSet<String> = HashSet::default();
-    reached.insert(from.to_string());
+    let mut reached: HashSet<String> = once(from.to_string()).collect();
 
     let mut t = 1;
+    // Loop until we have reached everything.
     while times.len() < connections.len() {
-        reached = reached_from(&connections, &reached);
+        reached = reached_from(connections, &reached);
 
         // Find if something can be reached that couldn't before.
         for loc in connections.keys() {
@@ -91,18 +82,18 @@ fn shortests_time_from(paths: &[Vec<String>], from: &str) -> usize {
     times.values().sum()
 }
 
-fn part3(paths: &[Vec<String>]) -> usize {
-    shortests_time_from(paths, "STT")
+fn part3(connections: &Connections) -> usize {
+    shortests_time_from(connections, "STT")
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let paths = build(&input);
+    let connections = create_connection_map(&input);
 
-    println!("Part 1: {}", unique_locations_count(&paths));
-    println!("Part 2: {}", part2(&paths));
-    println!("Part 3: {}", part3(&paths));
+    println!("Part 1: {}", unique_locations_count(&connections));
+    println!("Part 2: {}", part2(&connections));
+    println!("Part 3: {}", part3(&connections));
 }
 
 #[cfg(test)]
@@ -113,16 +104,19 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(unique_locations_count(&build(INPUT_TEST)), 7);
+        assert_eq!(
+            unique_locations_count(&create_connection_map(INPUT_TEST)),
+            7
+        );
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&build(INPUT_TEST)), 6);
+        assert_eq!(part2(&create_connection_map(INPUT_TEST)), 6);
     }
 
     #[test]
     fn test_part3() {
-        assert_eq!(part3(&build(INPUT_TEST)), 15);
+        assert_eq!(part3(&create_connection_map(INPUT_TEST)), 15);
     }
 }
