6d35b1358eed5b9711d5d9ed4a9fcd439de11216
Vincent Oberle <vincent@oberle.org>
2025-04-23 18:37:14 +0300

    Codyssi: 2024/01: Part 2

--- codyssi/2024/puzzle01/src/main.rs
+++ codyssi/2024/puzzle01/src/main.rs
@@ -8,12 +8,19 @@ fn part1(prices: &[u64]) -> u64 {
     prices.iter().sum()
 }
 
+fn part2(prices: &[u64], items_for_free_cnt: usize) -> u64 {
+    let mut sorted = prices.to_vec();
+    sorted.sort_unstable();
+    sorted[..sorted.len() - items_for_free_cnt].iter().sum()
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let prices = build(&input);
 
     println!("Part 1: {}", part1(&prices));
+    println!("Part 2: {}", part2(&prices, 20));
 }
 
 #[cfg(test)]
@@ -23,7 +30,12 @@ mod tests {
     const INPUT_TEST: &str = include_str!("../resources/input_test_1");
 
     #[test]
-    fn test_answer() {
+    fn test_part1() {
         assert_eq!(part1(&build(INPUT_TEST)), 2895391);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&build(INPUT_TEST), 2), 1261624);
     }
 }
