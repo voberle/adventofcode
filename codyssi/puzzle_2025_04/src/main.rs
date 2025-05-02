25a89d7b46b861300b5d03a61b070f35912e7351
Vincent Oberle <vincent@oberle.org>
2025-05-02 20:20:02 +0300

    Codyssi: 2025/04: Part 2

--- codyssi/puzzle_2025_04/src/main.rs
+++ codyssi/puzzle_2025_04/src/main.rs
@@ -1,14 +1,48 @@
 use std::io::{self, Read};
 
+fn str_to_vec(line: &str) -> Vec<char> {
+    line.chars().collect()
+}
+
 fn build(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|line| line.chars().collect()).collect()
+    input.lines().map(str_to_vec).collect()
+}
+
+fn char_to_memory_units(c: char) -> usize {
+    match c {
+        '0'..='9' => c.to_digit(10).unwrap() as usize,
+        'A'..='Z' => c as usize - 'A' as usize + 1,
+        _ => panic!("Unknown char"),
+    }
+}
+
+fn calc_memory_units(line: &[char]) -> usize {
+    line.iter().map(|&c| char_to_memory_units(c)).sum()
 }
 
 fn memory_units_count(message: &[Vec<char>]) -> usize {
+    message.iter().map(|line| calc_memory_units(line)).sum()
+}
+
+fn lossy_compress(line: &[char]) -> Vec<char> {
+    let to_keep_at_sides = line.len() / 10;
+    let len_middle = line.len() - to_keep_at_sides * 2;
+
+    let mut new_line = Vec::new();
+    new_line.extend_from_slice(&line[0..to_keep_at_sides]);
+    new_line.extend(len_middle.to_string().chars());
+    new_line.extend_from_slice(&line[line.len() - to_keep_at_sides..]);
+    new_line
+}
+
+fn memory_units_after_lossy_compression(message: &[Vec<char>]) -> usize {
     message
         .iter()
-        .flatten()
-        .map(|&c| c as usize - 'A' as usize + 1)
+        .map(|line| {
+            let compressed = lossy_compress(line);
+            // println!("{} =>  {}", line.iter().collect::<String>(), compressed.iter().collect::<String>());
+            calc_memory_units(&compressed)
+        })
         .sum()
 }
 
@@ -18,6 +52,7 @@ fn main() {
     let message = build(&input);
 
     println!("Part 1: {}", memory_units_count(&message));
+    println!("Part 2: {}", memory_units_after_lossy_compression(&message));
 }
 
 #[cfg(test)]
@@ -27,8 +62,27 @@ mod tests {
     const INPUT_TEST: &str = include_str!("../resources/input_test_1");
 
     #[test]
+    fn test_lossy_compress() {
+        assert_eq!(lossy_compress(&str_to_vec("ABCDEFGHIJ")), str_to_vec("A8J"));
+        assert_eq!(
+            lossy_compress(&str_to_vec("OONNHHHHHANNNHHHHHHHH")),
+            str_to_vec("OO17HH")
+        );
+        assert_eq!(
+            lossy_compress(&str_to_vec("BDGGGSCLUUVLCBBBQNUUUFFFFFXXXXXXXXX")),
+            str_to_vec("BDG29XXX")
+        );
+    }
+
+    #[test]
     fn test_part1() {
         let message = build(&INPUT_TEST);
         assert_eq!(memory_units_count(&message), 1247);
+    }
+
+    #[test]
+    fn test_part2() {
+        let message = build(&INPUT_TEST);
+        assert_eq!(memory_units_after_lossy_compression(&message), 219);
     }
 }
