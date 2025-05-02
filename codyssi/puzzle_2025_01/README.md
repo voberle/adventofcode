65a6f733458b0465285203440789727e12f59e22
Vincent Oberle <vincent@oberle.org>
2025-05-02 16:57:59 +0300

    Codyssi: 2025/01: Cleanups

--- codyssi/puzzle_2025_01/README.md
+++ codyssi/puzzle_2025_01/README.md
@@ -1,3 +1,3 @@
 # Puzzle 1: [Compass Calibration!](https://www.codyssi.com/view_problem_5?)
 
-
+Simple stuff with `zip()` and `fold()` iterator functions.
--- codyssi/puzzle_2025_01/src/main.rs
+++ codyssi/puzzle_2025_01/src/main.rs
@@ -12,43 +12,46 @@ fn build(input: &str) -> (Vec<u32>, Vec<char>) {
     (numbers, symbols)
 }
 
-fn actual_compass_offset(numbers: &[u32], symbols: &[char]) -> i64 {
+fn accumulate(acc: i64, number: i64, symbol: char) -> i64 {
+    match symbol {
+        '+' => acc + number,
+        '-' => acc - number,
+        _ => panic!("Unknown symbol"),
+    }
+}
+
+fn actual_offset(numbers: &[u32], symbols: &[char]) -> i64 {
     numbers
         .iter()
         .skip(1)
         .zip(symbols.iter())
-        .fold(i64::from(numbers[0]), |acc, (&n, s)| match s {
-            '+' => acc + i64::from(n),
-            '-' => acc - i64::from(n),
-            _ => panic!("Unknown symbol"),
+        .fold(i64::from(numbers[0]), |acc, (&n, &s)| {
+            accumulate(acc, i64::from(n), s)
         })
 }
 
-fn new_actual_compass_offset(numbers: &[u32], symbols: &[char]) -> i64 {
+fn new_offset(numbers: &[u32], symbols: &[char]) -> i64 {
     numbers
         .iter()
         .skip(1)
         .zip(symbols.iter().rev())
-        .fold(i64::from(numbers[0]), |acc, (&n, s)| match s {
-            '+' => acc + i64::from(n),
-            '-' => acc - i64::from(n),
-            _ => panic!("Unknown symbol"),
+        .fold(i64::from(numbers[0]), |acc, (&n, &s)| {
+            accumulate(acc, i64::from(n), s)
         })
 }
 
-fn final_actual_compass_offset(numbers: &[u32], symbols: &[char]) -> i64 {
+fn final_offset(numbers: &[u32], symbols: &[char]) -> i64 {
     fn digits_to_number(digits: &[u32]) -> i64 {
         i64::from(digits[0] * 10 + digits[1])
     }
 
-    numbers.chunks(2).skip(1).zip(symbols.iter().rev()).fold(
-        digits_to_number(&numbers[0..2]),
-        |acc, (digits, s)| match s {
-            '+' => acc + digits_to_number(digits),
-            '-' => acc - digits_to_number(digits),
-            _ => panic!("Unknown symbol"),
-        },
-    )
+    numbers
+        .chunks(2)
+        .skip(1)
+        .zip(symbols.iter().rev())
+        .fold(digits_to_number(&numbers[0..2]), |acc, (digits, &s)| {
+            accumulate(acc, digits_to_number(digits), s)
+        })
 }
 
 fn main() {
@@ -56,12 +59,9 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let (numbers, symbols) = build(&input);
 
-    println!("Part 1: {}", actual_compass_offset(&numbers, &symbols));
-    println!("Part 2: {}", new_actual_compass_offset(&numbers, &symbols));
-    println!(
-        "Part 3: {}",
-        final_actual_compass_offset(&numbers, &symbols)
-    );
+    println!("Part 1: {}", actual_offset(&numbers, &symbols));
+    println!("Part 2: {}", new_offset(&numbers, &symbols));
+    println!("Part 3: {}", final_offset(&numbers, &symbols));
 }
 
 #[cfg(test)]
@@ -73,18 +73,18 @@ mod tests {
     #[test]
     fn test_part1() {
         let (numbers, symbols) = build(&INPUT_TEST);
-        assert_eq!(actual_compass_offset(&numbers, &symbols), 21);
+        assert_eq!(actual_offset(&numbers, &symbols), 21);
     }
 
     #[test]
     fn test_part2() {
         let (numbers, symbols) = build(&INPUT_TEST);
-        assert_eq!(new_actual_compass_offset(&numbers, &symbols), 23);
+        assert_eq!(new_offset(&numbers, &symbols), 23);
     }
 
     #[test]
     fn test_part3() {
         let (numbers, symbols) = build(&INPUT_TEST);
-        assert_eq!(final_actual_compass_offset(&numbers, &symbols), 189);
+        assert_eq!(final_offset(&numbers, &symbols), 189);
     }
 }
