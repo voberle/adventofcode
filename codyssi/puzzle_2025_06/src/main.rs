75bbd348520613917dd0c0cb3b81dd44d5540be0
Vincent Oberle <vincent@oberle.org>
2025-05-03 21:01:15 +0300

    Codyssi: 2025/06: Part 3

--- codyssi/puzzle_2025_06/src/main.rs
+++ codyssi/puzzle_2025_06/src/main.rs
@@ -8,14 +8,38 @@ fn uncorrupted_count(log: &[char]) -> usize {
     log.iter().filter(|c| c.is_ascii_alphabetic()).count()
 }
 
-fn uncorrupted_values_sum(log: &[char]) -> usize {
-    log.iter()
-        .map(|&c| match c {
-            'a'..='z' => c as usize - 'a' as usize + 1,
-            'A'..='Z' => c as usize - 'A' as usize + 27,
-            _ => 0,
-        })
-        .sum()
+fn value_of(c: char) -> Option<i64> {
+    match c {
+        'a'..='z' => Some(c as i64 - 'a' as i64 + 1),
+        'A'..='Z' => Some(c as i64 - 'A' as i64 + 27),
+        _ => None,
+    }
+}
+
+fn uncorrupted_values_sum(log: &[char]) -> i64 {
+    log.iter().map(|&c| value_of(c).unwrap_or(0)).sum()
+}
+
+fn total_value(log: &[char]) -> i64 {
+    let mut values = Vec::new();
+    for c in log {
+        let value = if let Some(val) = value_of(*c) {
+            val
+        } else {
+            // Previous char value.
+            let mut val = *values.last().unwrap();
+            val = val * 2 - 5;
+            while val < 1 {
+                val += 52;
+            }
+            while val > 52 {
+                val -= 52;
+            }
+            val
+        };
+        values.push(value);
+    }
+    values.iter().sum()
 }
 
 fn main() {
@@ -25,6 +49,7 @@ fn main() {
 
     println!("Part 1: {}", uncorrupted_count(&log));
     println!("Part 2: {}", uncorrupted_values_sum(&log));
+    println!("Part 3: {}", total_value(&log));
 }
 
 #[cfg(test)]
@@ -43,5 +68,11 @@ mod tests {
     fn test_part2() {
         let log = build(&INPUT_TEST);
         assert_eq!(uncorrupted_values_sum(&log), 1742);
+    }
+
+    #[test]
+    fn test_part3() {
+        let log = build(&INPUT_TEST);
+        assert_eq!(total_value(&log), 2708);
     }
 }
