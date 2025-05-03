25de73b342ade3293cb9518b5942c9969242a719
Vincent Oberle <vincent@oberle.org>
2025-05-03 21:30:18 +0300

    Codyssi: 2025/07: Part 2

--- codyssi/puzzle_2025_07/src/main.rs
+++ codyssi/puzzle_2025_07/src/main.rs
@@ -10,27 +10,41 @@ fn build(input: &str) -> (Vec<u32>, Vec<(usize, usize)>, usize) {
             (p[0], p[1])
         })
         .collect();
-    let test_index = parts[2].parse().unwrap();
-    (frequencies, instructions, test_index)
+    let index = parts[2].parse().unwrap();
+    (frequencies, instructions, index)
 }
 
-fn test_track_freq(frequencies: &[u32], instructions: &[(usize, usize)], test_index: usize) -> u32 {
+fn track_freq_1(frequencies: &[u32], instructions: &[(usize, usize)], index: usize) -> u32 {
     let mut freqs = frequencies.to_vec();
-    for ins in instructions {
-        freqs.swap(ins.0 - 1, ins.1 - 1);
+    for (x, y) in instructions {
+        freqs.swap(x - 1, y - 1);
     }
-    freqs[test_index - 1]
+    freqs[index - 1]
+}
+
+fn track_freq_2(frequencies: &[u32], instructions: &[(usize, usize)], index: usize) -> u32 {
+    let mut freqs = frequencies.to_vec();
+    for index in 0..instructions.len() {
+        let (x, y, z) = (
+            instructions[index].0 - 1,
+            instructions[index].1 - 1,
+            instructions[(index + 1).rem_euclid(instructions.len())].0 - 1,
+        );
+        let z_val = freqs[z];
+        freqs[z] = freqs[y];
+        freqs[y] = freqs[x];
+        freqs[x] = z_val;
+    }
+    freqs[index - 1]
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let (frequencies, instructions, test_index) = build(&input);
+    let (freqs, instructions, index) = build(&input);
 
-    println!(
-        "Part 1: {}",
-        test_track_freq(&frequencies, &instructions, test_index)
-    );
+    println!("Part 1: {}", track_freq_1(&freqs, &instructions, index));
+    println!("Part 2: {}", track_freq_2(&freqs, &instructions, index));
 }
 
 #[cfg(test)]
@@ -41,7 +55,13 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let (frequencies, instructions, test_index) = build(&INPUT_TEST);
-        assert_eq!(test_track_freq(&frequencies, &instructions, test_index), 45);
+        let (freqs, instructions, index) = build(&INPUT_TEST);
+        assert_eq!(track_freq_1(&freqs, &instructions, index), 45);
+    }
+
+    #[test]
+    fn test_part2() {
+        let (freqs, instructions, index) = build(&INPUT_TEST);
+        assert_eq!(track_freq_2(&freqs, &instructions, index), 796);
     }
 }
