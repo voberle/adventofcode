0b51688d08a7107ee070015ec2a6d59ce0e57016
Vincent Oberle <vincent@oberle.org>
2025-05-03 20:50:25 +0300

    Codyssi: 2025/06: Part 2

--- codyssi/puzzle_2025_06/src/main.rs
+++ codyssi/puzzle_2025_06/src/main.rs
@@ -8,12 +8,23 @@ fn uncorrupted_count(log: &[char]) -> usize {
     log.iter().filter(|c| c.is_ascii_alphabetic()).count()
 }
 
+fn uncorrupted_values_sum(log: &[char]) -> usize {
+    log.iter()
+        .map(|&c| match c {
+            'a'..='z' => c as usize - 'a' as usize + 1,
+            'A'..='Z' => c as usize - 'A' as usize + 27,
+            _ => 0,
+        })
+        .sum()
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let log = build(&input);
 
     println!("Part 1: {}", uncorrupted_count(&log));
+    println!("Part 2: {}", uncorrupted_values_sum(&log));
 }
 
 #[cfg(test)]
@@ -26,5 +37,11 @@ mod tests {
     fn test_part1() {
         let log = build(&INPUT_TEST);
         assert_eq!(uncorrupted_count(&log), 59);
+    }
+
+    #[test]
+    fn test_part2() {
+        let log = build(&INPUT_TEST);
+        assert_eq!(uncorrupted_values_sum(&log), 1742);
     }
 }
