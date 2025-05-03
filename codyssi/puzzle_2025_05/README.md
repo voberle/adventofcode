7ae6c9d2d148df36d4605a36ac09d05208e39ce0
Vincent Oberle <vincent@oberle.org>
2025-05-03 12:27:11 +0300

    Codyssi: 2025/05: Part 2

--- codyssi/puzzle_2025_05/README.md
+++ codyssi/puzzle_2025_05/README.md
@@ -1,2 +1,3 @@
 # Puzzle 5: [Patron Islands!](https://www.codyssi.com/view_problem_9?)
 
+Part 2 is not very elegant, since I'm cloning twice the list.
--- codyssi/puzzle_2025_05/src/main.rs
+++ codyssi/puzzle_2025_05/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{self, Read};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Coord {
     x: i32,
     y: i32,
@@ -17,11 +18,15 @@ impl Coord {
         }
     }
 
-    fn dist_from_ship(&self) -> i32 {
-        self.x.abs() + self.y.abs()
+    fn ship() -> Self {
+        Self { x: 0, y: 0 }
     }
 
-    fn dist_from(&self, other: &Self) -> u32 {
+    fn dist_from_ship(self) -> u32 {
+        u32::try_from(self.x.abs() + self.y.abs()).unwrap()
+    }
+
+    fn dist_from(self, other: Self) -> u32 {
         self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
     }
 }
@@ -30,10 +35,31 @@ fn build(input: &str) -> Vec<Coord> {
     input.lines().map(Coord::build).collect()
 }
 
-fn diff_closest_furthest(islands: &[Coord]) -> i32 {
+fn diff_closest_furthest(islands: &[Coord]) -> u32 {
     let closest = islands.iter().min_by_key(|c| c.dist_from_ship()).unwrap();
     let furthest = islands.iter().max_by_key(|c| c.dist_from_ship()).unwrap();
     furthest.dist_from_ship() - closest.dist_from_ship()
+}
+
+fn sort_by_dist_from(islands: &[Coord], ref_island: Coord) -> Vec<Coord> {
+    let mut sorted = islands.to_vec();
+    sorted.sort_by(|a, b| {
+        let a_dist = ref_island.dist_from(*a);
+        let b_dist = ref_island.dist_from(*b);
+        a_dist.cmp(&b_dist).then(a.x.cmp(&b.x)).then(a.y.cmp(&b.y))
+    });
+    sorted
+}
+
+fn dist_closest_next_closests(islands: &[Coord]) -> u32 {
+    let sorted_from_ship = sort_by_dist_from(islands, Coord::ship());
+    let closest = sorted_from_ship.first().unwrap();
+
+    let islands_minus_closest: Vec<_> = islands.iter().filter(|&c| c != closest).copied().collect();
+    let sorted_from_closest = sort_by_dist_from(&islands_minus_closest, *closest);
+    let closest_from_closest = sorted_from_closest.first().unwrap();
+
+    closest.dist_from(*closest_from_closest)
 }
 
 fn main() {
@@ -42,6 +68,7 @@ fn main() {
     let islands = build(&input);
 
     println!("Part 1: {}", diff_closest_furthest(&islands));
+    println!("Part 2: {}", dist_closest_next_closests(&islands));
 }
 
 #[cfg(test)]
@@ -54,5 +81,11 @@ mod tests {
     fn test_part1() {
         let islands = build(&INPUT_TEST);
         assert_eq!(diff_closest_furthest(&islands), 226);
+    }
+
+    #[test]
+    fn test_part2() {
+        let islands = build(&INPUT_TEST);
+        assert_eq!(dist_closest_next_closests(&islands), 114);
     }
 }
