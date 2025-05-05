ce15aad3ab599293c02cd9871e0cc62487aadfc7
Vincent Oberle <vincent@oberle.org>
2025-05-05 16:50:54 +0300

    Codyssi: 2025/09: Part 3

--- codyssi/puzzle_2025_09/README.md
+++ codyssi/puzzle_2025_09/README.md
@@ -1,2 +1,5 @@
 # Puzzle 9: [Windy Bargain!](https://www.codyssi.com/view_problem_13?)
 
+Here part 3 was definitively more work, to correctly store and manage the debts.
+
+On the reimbursement part, to avoid trouble with Rust borrow-checker, I separated identifying which debt to repay and actually repaying it.
--- codyssi/puzzle_2025_09/src/main.rs
+++ codyssi/puzzle_2025_09/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    fmt::Display,
     io::{self, Read},
 };
 
@@ -18,6 +19,12 @@ impl From<&str> for Transaction {
             to: sub_parts[1].to_string(),
             amount: parts[1].parse().unwrap(),
         }
+    }
+}
+
+impl Display for Transaction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} => {}: {}", self.from, self.to, self.amount)
     }
 }
 
@@ -51,20 +58,20 @@ fn apply_transactions(balances: &mut HashMap<String, i32>, transactions: &[Trans
     }
 }
 
-fn apply_transactions_limited(balances: &mut HashMap<String, i32>, transactions: &[Transaction]) {
-    for tr in transactions {
-        let amount = tr.amount.min(*balances.get_mut(&tr.from).unwrap());
-
-        *balances.get_mut(&tr.from).unwrap() -= amount;
-        *balances.get_mut(&tr.to).unwrap() += amount;
-    }
-}
-
 fn balances_sum(initial_balances: &HashMap<String, i32>, transactions: &[Transaction]) -> i32 {
     let mut balances = initial_balances.clone();
     apply_transactions(&mut balances, transactions);
 
     three_highest_balances_sum(&balances)
+}
+
+fn apply_transactions_limited(balances: &mut HashMap<String, i32>, transactions: &[Transaction]) {
+    for tr in transactions {
+        let amount = tr.amount.min(balances[&tr.from]);
+
+        *balances.get_mut(&tr.from).unwrap() -= amount;
+        *balances.get_mut(&tr.to).unwrap() += amount;
+    }
 }
 
 fn limited_balances_sum(
@@ -77,6 +84,118 @@ fn limited_balances_sum(
     three_highest_balances_sum(&balances)
 }
 
+// Finds what debt can be reimbursed.
+// Returns one debt to reimburse, but there might be more.
+fn identify_reimbursement(
+    balances: &HashMap<String, i32>,
+    debts: &HashMap<String, Vec<(String, i32)>>,
+) -> Option<(String, String, i32)> {
+    for (debtor, debtor_balance) in balances {
+        let debtor = debtor.as_str();
+        let debtor_balance = *debtor_balance;
+
+        if debtor_balance > 0 {
+            if let Some(recv_debts) = debts.get(debtor) {
+                if !recv_debts.is_empty() {
+                    let reimbursing_to = &recv_debts[0].0;
+                    let amount_owned = recv_debts[0].1;
+
+                    // Enough money to pay in full.
+                    if debtor_balance > amount_owned {
+                        // println!("{debtor} pays {reimbursing_to} in full ({amount_owned})",);
+                        return Some((
+                            debtor.to_string(),
+                            reimbursing_to.to_string(),
+                            amount_owned,
+                        ));
+                    }
+
+                    // Partial repayment only.
+                    // println!("{debtor} pays {reimbursing_to} partially, {debtor_balance}",);
+                    return Some((
+                        debtor.to_string(),
+                        reimbursing_to.to_string(),
+                        debtor_balance,
+                    ));
+                }
+            }
+        }
+    }
+    None
+}
+
+fn apply_transactions_with_debt(balances: &mut HashMap<String, i32>, transactions: &[Transaction]) {
+    // Debts that everybody owns, ranked. A Vec is used to rank the debts.
+    let mut debts: HashMap<String, Vec<(String, i32)>> = HashMap::default();
+
+    // println!("Initial Balances: {balances:?}\n");
+
+    for tr in transactions {
+        if tr.amount <= balances[&tr.from] {
+            // Normal transaction.
+            // println!("Normal transaction: {tr}");
+            *balances.get_mut(&tr.from).unwrap() -= tr.amount;
+            *balances.get_mut(&tr.to).unwrap() += tr.amount;
+        } else {
+            // Limited transaction.
+            // println!("Limited transaction: {}", tr);
+            let amount_to_transfer = balances[&tr.from];
+            let debt_taken = tr.amount - amount_to_transfer;
+
+            // Sender has 0 cash and debts.
+            *balances.get_mut(&tr.from).unwrap() = 0;
+
+            // Update sender's debts.
+            debts
+                .entry(tr.from.clone())
+                .and_modify(|e| {
+                    // If there are several debts to the same person, they are kept separate.
+                    e.push((tr.to.clone(), debt_taken));
+                })
+                .or_insert(vec![(tr.to.clone(), debt_taken)]);
+
+            // Receiver gets all cash of sender.
+            *balances.get_mut(&tr.to).unwrap() += amount_to_transfer;
+        }
+
+        // After each transaction, see if some debts can be reimbursed.
+        while let Some((debtor, reimbursing_to, amount_to_repay)) =
+            identify_reimbursement(balances, &debts)
+        {
+            // Pay who is owned.
+            *balances.get_mut(&reimbursing_to).unwrap() += amount_to_repay;
+
+            // Update debtor balance.
+            *balances.get_mut(&debtor).unwrap() -= amount_to_repay;
+
+            // Update debtor debts.
+            let recv_debts = debts.get_mut(&debtor).unwrap();
+            recv_debts[0].1 -= amount_to_repay;
+            if recv_debts[0].1 == 0 {
+                // Debt is fully reimbursed.
+                recv_debts.remove(0);
+            }
+        }
+
+        // println!("Balances: {balances:?}",);
+        // println!("Debts:");
+        // for (who_owns, debts_owned) in &debts {
+        //     println!("  {who_owns} owns {debts_owned:?}");
+        // }
+        // println!();
+    }
+}
+
+fn balances_with_debt(
+    initial_balances: &HashMap<String, i32>,
+    transactions: &[Transaction],
+) -> i32 {
+    let mut balances = initial_balances.clone();
+    apply_transactions_with_debt(&mut balances, transactions);
+
+    three_highest_balances_sum(&balances)
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
@@ -84,6 +203,7 @@ fn main() {
 
     println!("Part 1: {}", balances_sum(&balances, &transactions));
     println!("Part 2: {}", limited_balances_sum(&balances, &transactions));
+    println!("Part 3: {}", balances_with_debt(&balances, &transactions));
 }
 
 #[cfg(test)]
@@ -102,5 +222,11 @@ mod tests {
     fn test_part2() {
         let (balances, transactions) = build(&INPUT_TEST);
         assert_eq!(limited_balances_sum(&balances, &transactions), 2542);
+    }
+
+    #[test]
+    fn test_part3() {
+        let (balances, transactions) = build(&INPUT_TEST);
+        assert_eq!(balances_with_debt(&balances, &transactions), 2511);
     }
 }
