fa7c47cce4b24442f1441484c2a21d3529b75017
Vincent Oberle <vincent@oberle.org>
2025-12-04 15:59:17 +0200

    2025/Day 03: Optimize finding max digit in slice

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -7,18 +7,21 @@ fn build(input: &str) -> Vec<Vec<u8>> {
         .collect()
 }
 
-fn max_joltage_2_batteries(bank: &[u8]) -> u64 {
-    // How to do this with only one iterator?
-    let d1 = *bank[..bank.len() - 1].iter().max().unwrap();
-    let d1_pos = bank.iter().position(|v| *v == d1).unwrap();
-
-    let d2 = *bank[d1_pos + 1..].iter().max().unwrap();
-
-    u64::from(d1) * 10 + u64::from(d2)
-}
-
-fn total_2_batteries(banks: &[Vec<u8>]) -> u64 {
-    banks.iter().map(|bank| max_joltage_2_batteries(bank)).sum()
+// Finds the maximum value in the slice, and returns position of the first occurence and the value.
+fn find_max(bank: &[u8]) -> (usize, u8) {
+    let mut highest_pos = 0;
+    let mut highest_val = 0;
+    for (p, v) in bank.iter().enumerate() {
+        if *v > highest_val {
+            highest_val = *v;
+            highest_pos = p;
+        }
+        if highest_val == 9 {
+            // We can't find higher than 9.
+            break;
+        }
+    }
+    (highest_pos, highest_val)
 }
 
 fn max_joltage<const BATTERIES: usize>(bank: &[u8]) -> u64 {
@@ -33,13 +36,9 @@ fn max_joltage<const BATTERIES: usize>(bank: &[u8]) -> u64 {
         // space to find the remaining ones.
         let end = bank.len() - (BATTERIES - digit);
 
-        let highest_digit = *bank[start..=end].iter().max().unwrap();
+        let (d_relative_pos, highest_digit) = find_max(&bank[start..=end]);
 
-        // Update next position to start: Just after current max.
-        let d_relative_pos = bank[start..=end]
-            .iter()
-            .position(|v| *v == highest_digit)
-            .unwrap();
+        // Next position to start is just after current max.
         start = start + d_relative_pos + 1;
 
         max *= 10;
@@ -61,7 +60,7 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let banks = build(&input);
 
-    println!("Part 1: {}", total_2_batteries(&banks));
+    println!("Part 1: {}", total_joltage::<2>(&banks));
     println!("Part 2: {}", total_joltage::<12>(&banks));
 }
 
@@ -71,13 +70,24 @@ mod tests {
 
     const INPUT_TEST: &str = include_str!("../resources/input_test_1");
 
+    fn max_joltage_2_batteries(bank: &[u8]) -> u64 {
+        let d1 = *bank[..bank.len() - 1].iter().max().unwrap();
+        let d1_pos = bank.iter().position(|v| *v == d1).unwrap();
+        let d2 = *bank[d1_pos + 1..].iter().max().unwrap();
+        u64::from(d1) * 10 + u64::from(d2)
+    }
+
+    fn total_2_batteries(banks: &[Vec<u8>]) -> u64 {
+        banks.iter().map(|bank| max_joltage_2_batteries(bank)).sum()
+    }
+
     #[test]
-    fn test_part1() {
+    fn test_part1_old() {
         assert_eq!(total_2_batteries(&build(INPUT_TEST)), 357);
     }
 
     #[test]
-    fn test_part1_part2code() {
+    fn test_part1() {
         assert_eq!(total_joltage::<2>(&build(INPUT_TEST)), 357);
     }
 
